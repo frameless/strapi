fefb780a3730f81903756f5863886e0fe10c343d
Ali Amori Kadhim <ali@frameless.io>
2022-10-10 15:11:14 +0200

    build: prepare to deploy

--- packages/strapi-dashboard/config/plugins.js
+++ packages/strapi-dashboard/config/plugins.js
@@ -1,4 +1,4 @@
-module.exports = {
+module.exports = ({ env }) => ({
   ckeditor5: {
     enabled: true,
   },
@@ -14,4 +14,19 @@ module.exports = {
       },
     },
   },
-};
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
