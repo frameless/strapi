1c8622c77e9f0307a5789d7e39986fd47d5c426b
Robbert Broersma <git@robbertbroersma.nl>
2022-10-12 15:51:13 +0200

    refactor: rename Strapi env variables

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
     locales: ['nl', 'en', "ar"]
   },
   images: {
-    domains: [process.env.STRAPI_HOSTNAME].filter(Boolean),
+    domains: [process.env.STRAPI_HOSTNAME],
   },
 }
 
