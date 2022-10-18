137a2a96cd0ffa6b4104d3fde612f71eff58177a
Ali Amori Kadhim <ali@frameless.io>
2022-10-18 12:32:03 +0200

    chore: improve alert component & add i18next

--- apps/frontend/pages/_app.tsx
+++ apps/frontend/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import { appWithTranslation } from "next-i18next";
 import { useRouter } from "next/router";
 import React from "react";
 import "@utrecht/component-library-css";
@@ -20,4 +21,4 @@ function MyApp({ Component, pageProps }: AppProps) {
   );
 }
 
-export default MyApp;
+export default appWithTranslation(MyApp);
