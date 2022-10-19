2a8911ea9f07d17fd906c773ab59678fabb7cb76
Ali Amori Kadhim <ali@frameless.io>
2022-10-19 14:54:08 +0200

    chore: build fag section component

---
+++ apps/frontend/components/FAQSection/index.tsx
@@ -0,0 +1,30 @@
+import { Heading2 } from "@utrecht/component-library-react";
+import { Markdown } from "../Markdown";
+import classnames from "classnames/bind";
+
+import { Accordion } from "../Accordion";
+
+import styles from "./index.module.css";
+
+type AccordionType = {
+  id: string;
+  title: string;
+  body: string;
+};
+
+export interface FAQSectionProps {
+  sectionTitle?: string;
+  accordion?: AccordionType[];
+}
+const cx = classnames.bind(styles);
+
+export const FAQSection: React.FC<FAQSectionProps> = ({ sectionTitle, accordion }) => (
+  <section className={cx("utrecht-faq-section")}>
+    <Heading2>{sectionTitle}</Heading2>
+    {accordion &&
+      accordion.length > 0 &&
+      accordion.map(({ id, title, body }: any) => (
+        <Accordion key={id} label={title} body={<Markdown>{body}</Markdown>} />
+      ))}
+  </section>
+);
