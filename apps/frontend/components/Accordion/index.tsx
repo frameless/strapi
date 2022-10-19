d73b47950d51d859ef997cd2d0f05e1a4e8a6373
Ali Amori Kadhim <ali@frameless.io>
2022-10-19 14:53:21 +0200

    chore: build accordion component

---
+++ apps/frontend/components/Accordion/index.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { Button, Heading3 } from "@utrecht/component-library-react";
+import classnames from "classnames/bind";
+
+import styles from "./index.module.css";
+
+export interface AccordionProps {
+  label?: string;
+  body?: any;
+}
+
+const cx = classnames.bind(styles);
+
+export const Accordion: React.FC<AccordionProps> = ({ label, body }) => {
+  const [expanded, setExpanded] = useState(false);
+  return (
+    <div className={cx("utrecht-accordion")}>
+      <Heading3 className={cx("utrecht-accordion__label")}>
+        <Button
+          className={cx("utrecht-accordion__button")}
+          appearance="subtle-button"
+          aria-expanded={expanded}
+          aria-controls={`sect-${label}`}
+          id={`${label}-id`}
+          onClick={() => setExpanded(!expanded)}
+        >
+          {label}
+        </Button>
+      </Heading3>
+      {expanded && (
+        <div
+          id={`sect-${label}`}
+          role="region"
+          aria-labelledby={`${label}-id`}
+          className={cx("utrecht-accordion__body")}
+        >
+          {body}
+        </div>
+      )}
+    </div>
+  );
+};
