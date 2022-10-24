75b00501d9ca7ba65ec0e870e49878ec348ebc95
Ali Amori Kadhim <ali@frameless.io>
2022-10-24 16:52:47 +0200

    chore: get the vercel url in dynamic way

--- apps/frontend/next-sitemap.config.js
+++ apps/frontend/next-sitemap.config.js
@@ -1,5 +1,6 @@
+
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.FRONTEND_SITE_URL,
+    siteUrl: process.env.NODE_ENV === 'production' ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` : 'http://localhost:1234',
     generateRobotsTxt: true,
 }
