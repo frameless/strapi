279a6dc7d6392aa79d16cd2e70799d19a24cd5ee
Ali Amori Kadhim <ali@frameless.io>
2022-11-17 14:43:27 +0100

    build: align environment variable

--- apps/frontend/next-sitemap.config.js
+++ apps/frontend/next-sitemap.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.FRONTEND_SITE_URL,
+    // todo Deprecate FRONTEND_SITE_URL env variable when we remove it from vercel 
+    siteUrl: process.env.FRONTEND_SITE_URL || process.env.STRAPI_FRONTEND_URL,
     generateRobotsTxt: true,
 }
