ff6dcb9d80fae6f1011d2bf5b7c34a686baf2dcf
Ali Amori Kadhim <ali@frameless.io>
2022-11-17 13:36:23 +0100

    feat: refactor the xml generator
    - [x] use xmlbuilder2 package to create the xml
    - [x] convert the gemeente xml file to json

---
+++ packages/samenwerkende-catalogi/src/index.js
@@ -0,0 +1,178 @@
+const { mapKeys } = require('lodash');
+const axios = require('axios');
+const { create } = require('xmlbuilder2');
+const { formatJsonata } = require("@stedi/prettier-plugin-jsonata/dist/lib");
+var fs = require('fs');
+require('dotenv').config();
+const gemeente = require('@frameless/catalogi-data');
+
+const { createSchema, getPrefLabel } = require('./helpers')
+
+const path = require('path')
+const dir = './dist';
+const prefixMap = {
+  xsi: "http://www.w3.org/2001/XMLSchema-instance",
+  dcterms: "http://purl.org/dc/terms/",
+  overheid: "http://standaarden.overheid.nl/owms/terms/",
+  overheidproduct: "http://standaarden.overheid.nl/product/terms/",
+}
+const xmlnsPrefixMap = mapKeys(prefixMap, (_value, key) => `xmlns:${key}`)
+
+const fetchProductPrice = async () => {
+  try {
+    const { data } = await axios({
+      url: process.env.STRAPI_BACKEND_URL,
+      method: 'post',
+      data: {
+        query: `{
+          products(locale: "nl"){
+            data {
+              id
+              attributes {
+                title
+                excerpt
+                slug
+                locale
+                updatedAt
+                catalogiMeta {
+                  spatial {
+                    schema
+                    resourceIdentifier
+                  }
+                  authority {
+                    schema
+                    resourceIdentifier
+                  }
+                  audience{
+                    id
+                    type
+                  }
+                  onlineRequest{
+                    type
+                  }
+                }
+              }
+            }
+          }
+            }`,
+      }
+    })
+
+
+    const meta = data.data.products.data.map(({ attributes, id }) => {
+      const gemeenteSpatial = attributes.catalogiMeta.spatial.resourceIdentifier
+      const gemeenteAuthority = attributes.catalogiMeta.authority.resourceIdentifier
+
+      const prefLabelSpatial = getPrefLabel(gemeente.cv.value, attributes.catalogiMeta.spatial.resourceIdentifier)
+      const prefLabelAuthority = getPrefLabel(gemeente.cv.value, attributes.catalogiMeta.authority.resourceIdentifier)
+
+      const schema = createSchema("{http://standaarden.overheid.nl/owms/terms/}Gemeente", prefixMap)
+
+      const path = 'products' // can be from the CMS
+      const identifier = `${process.env.STRAPI_FRONTEND_URL}/${attributes.locale}/${path}/${attributes.slug}`
+
+      const spatial = {
+        schema,
+        resourceIdentifier: gemeenteSpatial,
+        label: prefLabelSpatial
+      }
+
+      const authority = {
+        schema: schema,
+        resourceIdentifier: gemeenteAuthority,
+        label: prefLabelAuthority,
+      }
+
+      const audiences = attributes.catalogiMeta.audience.map(({ id, type }) => ({ id, type, scheme: "overheid:Doelgroep" }))
+      return {
+        title: attributes.title,
+        language: attributes.locale,
+        modified: attributes.updatedAt,
+        productId: id,
+        abstract: attributes.excerpt,
+        onlineAanvragen: attributes.catalogiMeta.onlineRequest.type,
+        identifier,
+        spatial,
+        authority,
+        audiences
+      }
+    })
+
+
+    const root = create({ version: '1.0' })
+      .ele('overheidproduct:scproducten')
+      .att({
+        ...xmlnsPrefixMap,
+        'xsi:schemaLocation': "https://standaarden.overheid.nl/product/terms/sc.xsd"
+      })
+    meta.forEach((item) => {
+      const scproduct = root.ele('overheidproduct:scproduct').att({ 'owms-version': "4.0" })
+      const meta = scproduct.ele('overheidproduct:meta')
+      meta.ele('overheidproduct:owmskern')
+        .ele('dcterms:identifier').txt(item.identifier).up()
+        .ele('dcterms:title').txt(item.title).up()
+        .ele('dcterms:language').txt(item.language).up()
+        .ele('dcterms:type').att({ scheme: 'overheid:Informatietype' }).txt('productbeschrijving').up()
+        .ele('dcterms:modified').txt(item.modified).up()
+        .ele('dcterms:spatial').att({
+          scheme: item.spatial.schema,
+          resourceIdentifier: item.spatial.resourceIdentifier
+        })
+        .txt(item.spatial.label).up()
+        .ele('overheid:authority').att({
+          scheme: item.authority.schema,
+          resourceIdentifier: item.authority.resourceIdentifier
+        })
+        .txt(item.authority.label).up()
+      const owmsmantel = meta.ele('overheidproduct:owmsmantel')
+      item.audiences.forEach((audience) => {
+        owmsmantel.ele('dcterms:audience').att({
+          scheme: audience.scheme,
+          resourceIdentifier: `http://standaarden.overheid.nl/owms/terms/${audience?.type.toLowerCase()}`
+        }).txt(audience.type)
+      })
+      owmsmantel.ele('dcterms:abstract').txt(item.abstract).up()
+      meta.ele('overheidproduct:scmeta')
+        .ele('overheidproduct:productID').txt(item.productId).up()
+        .ele('overheidproduct:onlineAanvragen').txt(item.onlineAanvragen).up()
+      scproduct.ele('overheidproduct:body')
+    })
+    // convert the XML tree to string
+    const xml = root.end({ prettyPrint: true });
+    const jsonXML = root.doc().toObject()
+
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, {
+        recursive: true
+      });
+    }
+
+    fs.writeFileSync(`${dir}/sc.xml`, xml);
+    fs.writeFileSync(`${dir}/sc.json`, prettierFormateJson(jsonXML));
+  } catch (error) {
+    console.log(error);
+  }
+}
+fetchProductPrice();
+
+
+(function generateGemeenetJson() {
+  fs.readFile(require.resolve(path.resolve(process.cwd(), 'src/gemeente.xml')), (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, {
+        recursive: true
+      });
+    }
+
+    const doc = create(data.toString());
+    const gemeenteJson = doc.toObject()
+    fs.writeFileSync(`${dir}/gemeente.json`, prettierFormateJson(gemeenteJson));
+  })
+})()
+
+function prettierFormateJson(json) {
+  return formatJsonata(JSON.stringify(json), { printWidth: 120, tabWidth: 2, useTabs: false });
+}
