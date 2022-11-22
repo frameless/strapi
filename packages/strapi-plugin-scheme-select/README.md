cf3d9537daf06046e6f47dec164ba20347860916
Ali Amori Kadhim <ali@frameless.io>
2022-11-22 09:27:23 +0100

    feat: create strapi-plugin-scheme-select

---
+++ packages/strapi-plugin-scheme-select/README.md
@@ -0,0 +1,3 @@
+# Strapi plugin scheme-select
+
+A strapi custom field for selecting a scheme
---
+++ packages/strapi-plugin-scheme-select/admin/src/index.js
@@ -0,0 +1,72 @@
+import { prefixPluginTranslations } from '@strapi/helper-plugin';
+import pluginId from './pluginId';
+import ComboboxIcon from './components/ComboboxIcon';
+import getTrad from './utils/getTrad';
+
+export default {
+    register(app) {
+        app.customFields.register({
+            name: 'scheme',
+            pluginId: 'scheme-select',
+            type: 'string',
+            icon: ComboboxIcon,
+            intlLabel: {
+                id: getTrad('scheme-select.label'),
+                defaultMessage: 'Scheme',
+            },
+            intlDescription: {
+                id: getTrad('scheme-select.description'),
+                defaultMessage: 'Select any scheme',
+            },
+            components: {
+                Input: async () =>
+                    import('./components/CustomCombobox'),
+            },
+            options: {
+                advanced: [
+                    {
+                        sectionTitle: {
+                            id: 'global.settings',
+                            defaultMessage: 'Settings',
+                        },
+                        items: [
+                            {
+                                name: 'required',
+                                type: 'checkbox',
+                                intlLabel: {
+                                    id: 'form.attribute.item.requiredField',
+                                    defaultMessage: 'Required field',
+                                },
+                                description: {
+                                    id: 'form.attribute.item.requiredField.description',
+                                    defaultMessage: "You won't be able to create an entry if this field is empty",
+                                },
+                            },
+                        ],
+                    },
+                ],
+            },
+        });
+    },
+    async registerTrads({ locales }) {
+        const importedTrads = await Promise.all(
+            locales.map((locale) => {
+                return import(`./translations/${locale}.json`)
+                    .then(({ default: data }) => {
+                        return {
+                            data: prefixPluginTranslations(data, pluginId),
+                            locale,
+                        };
+                    })
+                    .catch(() => {
+                        return {
+                            data: {},
+                            locale,
+                        };
+                    });
+            })
+        );
+
+        return Promise.resolve(importedTrads);
+    },
+};
