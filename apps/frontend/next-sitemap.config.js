77f07d93c41402fedad5a90f0215c34b77f1383e
Ali Amori Kadhim <ali@frameless.io>
2023-03-23 12:30:23 +0100

    chore: enable sitemap on server-side

--- apps/frontend/next-sitemap.config.js
+++ apps/frontend/next-sitemap.config.js
@@ -2,4 +2,8 @@
 module.exports = {
   siteUrl: process.env.STRAPI_FRONTEND_URL,
   generateRobotsTxt: true,
+  exclude: ['/server-sitemap.xml'],
+  robotsTxtOptions: {
+    additionalSitemaps: [`${process.env.STRAPI_FRONTEND_URL}/server-sitemap.xml`],
+  },
 };
