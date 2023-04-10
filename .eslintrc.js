0fe7411fbb6b0288f2d67e66f792edd036a2f273
Ali Amori Kadhim <ali@frameless.io>
2023-04-10 10:34:23 +0200

    build: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,64 @@
+/* eslint-env node */
+
+module.exports = {
+  globals: {
+    process: true,
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: false,
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  overrides: [
+    {
+      extends: ['plugin:json/recommended'],
+      files: ['*.json'],
+    },
+    {
+      extends: [
+        'plugin:react/recommended',
+        'eslint-config-prettier',
+        '@strapi-community',
+        './.eslintrc.js.json',
+        './.eslintrc.react.json',
+      ],
+      files: ['*.js', '*.jsx'],
+      plugins: ['import', 'jest'],
+    },
+    {
+      extends: ['plugin:react/recommended', 'eslint-config-prettier', './.eslintrc.js.json', './.eslintrc.react.json'],
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint', 'import', 'jest'],
+    },
+    {
+      extends: ['plugin:react/recommended', 'eslint-config-prettier', './.eslintrc.js.json'],
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: [
+          './apps/frontend/tsconfig.json',
+          './apps/strapi-dashboard/tsconfig.json',
+          './apps/strapi-dashboard/src/admin/tsconfig.json',
+          './packages/catalogi-data/tsconfig.json',
+          './packages/samenwerkende-catalogi/tsconfig.json',
+        ],
+        tsconfigRootDir: __dirname,
+      },
+      plugins: ['@typescript-eslint', 'import'],
+    },
+  ],
+};
