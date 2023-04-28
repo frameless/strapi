0f7d087d6333de00c8803a72204bb46bab6ef3ea
Ali Amori Kadhim <ali@frameless.io>
2023-04-28 09:24:07 +0200

    chore: enable scss in strapi-dashboard

--- apps/strapi-dashboard/src/admin/webpack.config.js
+++ apps/strapi-dashboard/src/admin/webpack.config.js
@@ -1,7 +1,26 @@
 'use strict';
+
 module.exports = (config, webpack) => {
   config.plugins.push(
     new webpack.NormalModuleReplacementPlugin(/^tippy\.js$/, 'tippy.js/dist/tippy-bundle.umd.min.js'),
   );
+  // Allow scss modules
+  config.resolve = {
+    ...config.resolve,
+    extensions: [...config.resolve.extensions, '.scss'],
+  };
+
+  // Configure a SASS loader
+  config.module.rules.push({
+    test: /\.s[ac]ss$/i,
+    use: [
+      'style-loader',
+      'css-loader',
+      'sass-loader',
+      {
+        loader: 'sass-loader',
+      },
+    ],
+  });
   return config;
 };
