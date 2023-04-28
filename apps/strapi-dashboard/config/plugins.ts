578163b007800d3409706a6198621ccdf61aef03
Ali Amori Kadhim <ali@frameless.io>
2023-04-28 09:25:12 +0200

    chore: add strapi-prometheus plugin to strapi-dashboard

--- apps/strapi-dashboard/config/plugins.ts
+++ apps/strapi-dashboard/config/plugins.ts
@@ -1,9 +1,23 @@
+const { apolloPrometheusPlugin } = require('strapi-prometheus');
+
 export default ({ env }) => ({
   ckeditor5: {
     enabled: false,
   },
   'strapi-tiptap-editor': {
     enabled: true,
+  },
+  'strapi-prometheus': {
+    enabled: true,
+    graphql: {
+      enabled: true,
+      config: {
+        apolloServer: {
+          plugins: [apolloPrometheusPlugin], // add the plugin to get apollo metrics
+          tracing: true, // this must be true to get some of the data needed to create the metrics
+        },
+      },
+    },
   },
   slugify: {
     enabled: true,
