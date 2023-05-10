93f65b435cd2a7ae73dc098fe1002fc63b957858
Ali Amori Kadhim <ali@frameless.io>
2023-05-10 15:21:17 +0200

    chore: remove ckeditor5

--- packages/strapi-plugin-ckeditor5/admin/src/pages/HomePage/index.js
+++
@@ -1,5 +0,0 @@
-function HomePage() {
-  return null;
-}
-
-export default HomePage;
--- packages/strapi-plugin-ckeditor5/server/controllers/my-controller.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = ({ strapi }) => ({
-  index(ctx) {
-    ctx.body = strapi.plugin('wysiwyg').service('myService').getWelcomeMessage();
-  },
-});
