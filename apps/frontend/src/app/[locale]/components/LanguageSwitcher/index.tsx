b06e5abbe7bbea5280894e0c0eda9d99cc74c355
Ali Amori Kadhim <ali@frameless.io>
2023-06-07 16:20:43 +0200

    chore: fix the language switcher layout jumping issue

--- apps/frontend/src/app/[locale]/components/LanguageSwitcher/index.tsx
+++ apps/frontend/src/app/[locale]/components/LanguageSwitcher/index.tsx
@@ -13,30 +13,35 @@ type LanguageSwitcherItemsType = { pathname: string; locale: Locale };
 interface LanguageSwitcherProps {
   items: LanguageSwitcherItemsType[];
   currentLocale: Locale;
+  loading?: boolean;
 }
 
-export const LanguageSwitcher: React.FC<LanguageSwitcherProps> = ({ items, currentLocale }) => (
+export const LanguageSwitcher: React.FC<LanguageSwitcherProps> = ({ items, currentLocale, loading }) => (
   <div className="utrecht-language-switcher">
     {items &&
       items
         .sort((a, b) => a.locale.localeCompare(b.locale))
-        .map(({ locale, pathname }) => (
-          <span key={locale} className="utrecht-language-switcher_item">
-            <Link
-              href={pathname}
-              locale={locale}
-              className={clsx('utrecht-link', {
-                'utrecht-language-switcher_item--current': locale === currentLocale,
-              })}
-              hrefLang={locale}
-              lang={locale}
-              rel={locale !== currentLocale ? 'alternate' : ''}
-              title={mappedLocales[locale as T]}
-            >
-              {locale.toUpperCase()}
-            </Link>
-          </span>
-        ))}
+        .map(({ locale, pathname }) =>
+          loading ? (
+            <LanguageSwitcherSkeleton key={locale} />
+          ) : (
+            <span key={locale} className="utrecht-language-switcher_item">
+              <Link
+                href={pathname}
+                locale={locale}
+                className={clsx('utrecht-link', {
+                  'utrecht-language-switcher_item--current': locale === currentLocale,
+                })}
+                hrefLang={locale}
+                lang={locale}
+                rel={locale !== currentLocale ? 'alternate' : ''}
+                title={mappedLocales[locale as T]}
+              >
+                {locale.toUpperCase()}
+              </Link>
+            </span>
+          ),
+        )}
   </div>
 );
 
@@ -44,8 +49,8 @@ export const LanguageSwitcherSkeleton = () => (
   <div className="utrecht-language-switcher__skeleton">
     <Skeleton
       containerClassName="utrecht-language-switcher__skeleton-container"
-      width="40%"
       duration={1000}
+      height="100%"
       className="utrecht-language-switcher__skeleton-body"
     />
   </div>
