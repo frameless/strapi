e2fe2b11df02e3449e71feff2fc27baa95ba78db
Ali Amori Kadhim <ali@frameless.io>
2023-06-09 14:48:44 +0200

    chore: add single type for not found page

---
+++ apps/strapi-dashboard/src/api/not-found-page/controllers/not-found-page.ts
@@ -0,0 +1,7 @@
+/**
+ * not-found-page controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::not-found-page.not-found-page');
---
+++ apps/strapi-dashboard/src/api/not-found-page/routes/not-found-page.ts
@@ -0,0 +1,7 @@
+/**
+ * not-found-page router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::not-found-page.not-found-page');
