bfaec3aca55beec17cad91e6e815dc45b900785a
Bryan de Jong <bryandejong99@gmail.com>
2023-07-21 10:47:55 +0100

    feat: render visualisation spec from graphql query

--- apps/frontend/src/app/[locale]/themas/page.tsx
+++ apps/frontend/src/app/[locale]/themas/page.tsx
@@ -1,12 +1,22 @@
-import { VegaVisualisation } from "@/components/VegaVisualisation";
-import exampleSpec1 from "./exampleSpec1";
+import { VegaVisualisation } from '@/components/VegaVisualisation';
+import { GET_THEMAS } from '@/query';
+import { fetchData } from '@/util/fetchData';
+import { VisualizationSpec } from "react-vega";
 
+const Themas = async ({ params: { locale } }: { params: { locale: string } }) => {
+  const { data } = await fetchData({
+    url: process.env.STRAPI_BACKEND_URL as string,
+    query: GET_THEMAS,
+    variables: { locale: locale },
+  });
 
-const Themas = async () => {
+  const visualisations = data.themas.data[0].attributes.Visualisatie;
 
   return (
     <>
-      <VegaVisualisation spec={exampleSpec1} />
+      {visualisations.map((visualisation: { id: string, specification: VisualizationSpec }) => (
+        <VegaVisualisation spec={visualisation.specification} key={visualisation.id} />
+      ))}
     </>
   );
 };
