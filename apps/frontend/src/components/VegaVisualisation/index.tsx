049be3d1574460d264da86660a0ac8b6d405c090
Ali Amori Kadhim <ali@frameless.io>
2023-07-26 12:49:19 +0200

    chore: fix prettier formate issue

--- apps/frontend/src/components/VegaVisualisation/index.tsx
+++ apps/frontend/src/components/VegaVisualisation/index.tsx
@@ -1,11 +1,9 @@
 'use client';
 
-import { Vega, VisualizationSpec } from "react-vega";
+import { Vega, VisualizationSpec } from 'react-vega';
 
 interface VegaVisualisationProps {
   spec: VisualizationSpec;
 }
 
-export const VegaVisualisation = ({ spec }: VegaVisualisationProps) => (
-  <Vega spec={spec} />
-);
+export const VegaVisualisation = ({ spec }: VegaVisualisationProps) => <Vega spec={spec} />;
