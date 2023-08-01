d7298deb5d9dcc97a86b6c5b1227144f1dee5643
Ali Amori Kadhim <ali@frameless.io>
2023-08-01 14:36:24 +0200

    fix: extract hostname & protocol from the strapi image-url

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,7 +1,15 @@
+const { URL } = require('url');
+const { hostname, protocol, port } = new URL(process.env.STRAPI_IMAGE_URL || 'http://localhost:1337');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [process.env.STRAPI_UPLOAD_PROVIDER_HOSTNAME || ''],
+    remotePatterns: [
+      {
+        protocol: protocol.replace(/:$/, ''),
+        hostname: hostname,
+        port: port,
+      },
+    ],
   },
   experimental: {
     serverActions: true,
