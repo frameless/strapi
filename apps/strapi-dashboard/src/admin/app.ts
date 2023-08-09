8125607aeadc4f177571edef5f154a5dd03dd156
Ali Amori Kadhim <ali@frameless.io>
2023-08-09 14:26:07 +0200

    fix: add i18n missing keys to draft en publish (dutch)

--- apps/strapi-dashboard/src/admin/app.ts
+++ apps/strapi-dashboard/src/admin/app.ts
@@ -9,6 +9,12 @@ const config = {
     },
     nl: {
       'app.components.HomePage.welcome': 'Welkom bij het Utrecht PDC dashboard 👋',
+      'content-manager.popUpWarning.warning.has-draft-relations.title': 'Bevestiging',
+      'content-manager.popUpWarning.warning.publish-question': 'Wil je nog steeds publiceren?',
+      'content-manager.popUpWarning.warning.unpublish-question': 'Weet je zeker dat je het niet wilt publiceren?',
+      'content-manager.popUpWarning.warning.unpublish':
+        'Als je deze inhoud niet publiceert, wordt deze automatisch een concept.',
+      'content-manager.popUpWarning.warning.updateAllSettings': 'Dit zal al je instellingen wijzigen.',
     },
   },
 };
