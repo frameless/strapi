b850f8235534b25ac7efc03624256336c034847c
Bryan de Jong <bryandejong99@gmail.com>
2023-08-25 10:13:48 +0200

    feat: add homepage single-type

--- apps/vth-dashboard/src/api/thema-content/controllers/thema-content.ts
+++ apps/vth-dashboard/src/api/thema-content/controllers/thema-content.ts
@@ -2,6 +2,6 @@
  * thema-content controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
 export default factories.createCoreController('api::thema-content.thema-content');
