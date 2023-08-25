a82c6468792a55ef1a56293aae7390e6543d88e5
Bryan de Jong <bryandejong99@gmail.com>
2023-08-25 09:58:06 +0200

    feat: add thema,sub-thema collections

---
+++ apps/vth-dashboard/src/api/sub-thema/controllers/sub-thema.ts
@@ -0,0 +1,7 @@
+/**
+ * sub-thema controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::sub-thema.sub-thema');
---
+++ apps/vth-dashboard/src/api/sub-thema/routes/sub-thema.ts
@@ -0,0 +1,7 @@
+/**
+ * sub-thema router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::sub-thema.sub-thema');
