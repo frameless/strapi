d2b7b1485b05171cf5bc7035c1fbeb0cc1d3ba24
Ali Amori Kadhim <ali@frameless.io>
2023-09-29 10:21:52 +0200

    feat(pdc-frontend): integrate ui package

--- apps/pdc-frontend/src/components/index.tsx
+++ apps/pdc-frontend/src/components/index.tsx
@@ -3,3 +3,4 @@
 export * from '@utrecht/component-library-react/dist/css-module';
 export * from '@utrecht/web-component-library-react';
 export { PageTitle } from './Page-title';
+export * from '@frameless/ui';
