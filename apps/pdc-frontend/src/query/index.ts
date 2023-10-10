6a04a322b712f940ec87299f37cf240de5078fda
Ali Amori Kadhim <ali@frameless.io>
2023-10-10 14:50:21 +0200

    fix(pdc-frontend): remove `contentRole` from pdc-frontend

--- apps/pdc-frontend/src/query/index.ts
+++ apps/pdc-frontend/src/query/index.ts
@@ -139,7 +139,6 @@ export const GET_PRODUCT_BY_SLUG_FETCH = gql(`
             }
             ... on ComponentComponentsSpotlight {
               __typename
-              contentRole
               content
               type
             }
