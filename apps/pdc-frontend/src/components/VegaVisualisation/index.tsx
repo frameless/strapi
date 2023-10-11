550c49f59b6e1352bf160869548d839f7f8368cf
Ali Amori Kadhim <ali@frameless.io>
2023-10-11 14:22:24 +0200

    chore(pdc-frontend): remove visualisatie demo page

--- apps/pdc-frontend/src/components/VegaVisualisation/index.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { Vega, VisualizationSpec } from 'react-vega';
-
-interface VegaVisualisationProps {
-  spec: VisualizationSpec;
-}
-
-export const VegaVisualisation = ({ spec }: VegaVisualisationProps) => <Vega spec={spec} />;
