252686f43a7f930f7f64402c8b683c26d0a0034c
Bryan <bryandejong99@gmail.com>
2023-10-11 15:44:17 +0100

    chore(ui): remove dist from ui package

--- packages/ui/dist/src/components/Markdown/index.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Components } from 'react-markdown';
-interface MarkdownProps {
-    children: string;
-    components?: Components;
-}
-export declare const Markdown: React.FC<MarkdownProps>;
-export {};
--- packages/ui/dist/src/components/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './Navigation';
-export * from './Img';
-export * from './LogoButton';
-export * from './PriceWidget';
-export * from './Markdown';
-export * from './MultiColumnsLogoButton';
