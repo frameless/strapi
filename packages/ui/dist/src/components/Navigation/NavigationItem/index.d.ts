071005e8e53705840f1d730e21ce29040d27c747
Ali Amori Kadhim <ali@frameless.io>
2023-10-13 10:45:17 +0200

    chore(ui): remove the ui dist folder

--- packages/ui/dist/src/components/Navigation/NavigationItem/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { LiHTMLAttributes } from 'react';
-interface NavigationItemProps extends LiHTMLAttributes<HTMLLIElement> {
-    mobile?: boolean;
-}
-export declare const NavigationItem: import("react").ForwardRefExoticComponent<NavigationItemProps & {
-    children?: import("react").ReactNode;
-} & import("react").RefAttributes<HTMLLIElement>>;
-export {};
