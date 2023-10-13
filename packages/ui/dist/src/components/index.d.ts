e6966da3118bae9a561caa663d1e4ffab0b63f24
Ali Amori Kadhim <ali@frameless.io>
2023-10-13 10:45:17 +0200

    chore(ui): remove the ui dist folder

--- packages/ui/dist/src/components/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from './AdvancedLink';
-export * from './Navigation';
-export * from './Img';
-export * from './LogoButton';
-export * from './PriceWidget';
-export * from './Markdown';
-export * from './MultiColumnsLogoButton';
