07219098aed6a598395749d479664f42c9d15ca6
Ali Amori Kadhim <ali@frameless.io>
2023-10-17 12:59:02 +0200

    chore(deps): Update strapi dependencies
    include:
    - apps
    - packages
    - update strapi-patch

--- packages/strapi-tiptap-editor/server/controllers/settings.ts
+++ packages/strapi-tiptap-editor/server/controllers/settings.ts
@@ -3,16 +3,22 @@ import defaultSettings from '../../utils/defaults';
 
 export default ({ strapi }: { strapi: Strapi }) => ({
   async index(ctx) {
-    const savedSettings = await strapi.store({ type: 'plugin', name: 'strapi-tiptap-editor', key: 'settings' }).get();
-    if (savedSettings !== null) {
-      ctx.send(savedSettings);
-    } else {
-      ctx.send(defaultSettings);
+    if (strapi.store) {
+      const savedSettings = await strapi
+        .store({ type: 'plugin', name: 'strapi-tiptap-editor', key: 'settings' })
+        .get({});
+      if (savedSettings !== null) {
+        ctx.send(savedSettings);
+      } else {
+        ctx.send(defaultSettings);
+      }
     }
   },
   async updateSettings(ctx) {
-    const newSettings = ctx.request.body;
-    await strapi.store({ type: 'plugin', name: 'strapi-tiptap-editor', key: 'settings' }).set({ value: newSettings });
-    ctx.send({ res: 'ok' });
+    if (strapi.store) {
+      const newSettings = ctx.request.body;
+      await strapi.store({ type: 'plugin', name: 'strapi-tiptap-editor', key: 'settings' }).set({ value: newSettings });
+      ctx.send({ res: 'ok' });
+    }
   },
 });
