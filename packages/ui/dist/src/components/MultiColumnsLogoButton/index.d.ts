9419f08307c5aade97fd1a52f80d98e57961bb28
Bryan de Jong <bryandejong99@gmail.com>
2023-10-20 09:54:14 +0100

    chore: remove dist and add to gitignore

--- packages/ui/dist/src/components/MultiColumnsLogoButton/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { LogoButtonProps } from '../LogoButton';
-interface MultiColumnsButtonProps {
-    columns: {
-        title?: string;
-        logoButton: LogoButtonProps[];
-    }[];
-}
-export declare const MultiColumnsButton: ({ columns }: MultiColumnsButtonProps) => import("react/jsx-runtime").JSX.Element;
-export {};
--- packages/ui/dist/src/components/Navigation/NavigationIcon/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-interface NavigationIconProps {
-    name: string;
-    ariaHidden?: boolean;
-}
-export declare const NavigationIcon: import("react").ForwardRefExoticComponent<NavigationIconProps & import("react").RefAttributes<SVGSVGElement>>;
-export {};
