e3ba74effc4dd5513865d04b6a3a911070bb10bd
Bryan de Jong <bryandejong99@gmail.com>
2023-10-20 02:23:34 +0100

    feat: make whole card clickable

--- apps/vth-frontend/src/components/Card/index.tsx
+++ apps/vth-frontend/src/components/Card/index.tsx
@@ -1,7 +1,10 @@
+'use client';
+
 import { Heading2, Paragraph } from '@utrecht/component-library-react';
 import clsx from 'clsx';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import React, { FC, HTMLAttributes } from 'react';
 import './index.style.css';
 
@@ -18,13 +21,17 @@ interface CardProps extends Omit<HTMLAttributes<HTMLDivElement>, 'children'> {
 }
 
 export const Card: FC<CardProps> = ({ description, title, image: { url = '', alt }, link: { href }, ...props }) => {
+  const linkRef = React.useRef<HTMLAnchorElement>(null);
+
   return (
-    <div {...props} className={clsx('utrecht-card', props.className)}>
+    <div {...props} className={clsx('utrecht-card', props.className)} onClick={() => linkRef.current?.click()}>
       {url && <Image src={url} alt={alt} className={'utrecht-card__image'} width={312} height={200} />}
       <div className={'utrecht-card__content'}>
-        <Link href={href} className={'utrecht-link'}>
-          <Heading2 className="utrecht-card__title">{title}</Heading2>
-        </Link>
+        <Heading2 className="utrecht-card__title">
+          <Link ref={linkRef} href={href} className={'utrecht-link utrecht-link--html-a'}>
+            {title}
+          </Link>
+        </Heading2>
         <Paragraph className="utrecht-card__body">{description}</Paragraph>
       </div>
     </div>
