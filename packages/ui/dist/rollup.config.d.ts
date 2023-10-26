8017381c527bbaddc241a85b50b393c295472496
Ali Amori Kadhim <ali@frameless.io>
2023-10-26 16:46:34 +0200

    chore: remove dist folder

--- packages/ui/dist/rollup.config.d.ts
+++
@@ -1,7 +0,0 @@
-import { RollupOptions } from 'rollup';
-export declare const outputGlobals: {
-    react: string;
-    'react-dom': string;
-};
-declare const config: RollupOptions;
-export default config;
--- packages/ui/dist/src/hooks/useClickOutside.d.ts
+++
@@ -1,2 +0,0 @@
-import { RefObject } from 'react';
-export declare const useClickOutside: (ref: RefObject<HTMLElement>, callback: () => void) => void;
