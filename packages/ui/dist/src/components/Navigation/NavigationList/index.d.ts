558db773291ff4fd7bbdc0e3ec0ae10d90788b2c
Ali Amori Kadhim <ali@frameless.io>
2023-10-31 10:34:17 +0100

    chore: exclude dist folder

--- packages/ui/dist/src/components/Navigation/NavigationList/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { DetailedHTMLProps, HTMLAttributes, PropsWithChildren } from 'react';
-import { NavigationListType } from '../index';
-interface NavigationListProps extends DetailedHTMLProps<HTMLAttributes<HTMLUListElement>, HTMLUListElement> {
-    list: NavigationListType[];
-    mobile?: boolean;
-}
-export declare const NavigationList: import("react").ForwardRefExoticComponent<Omit<PropsWithChildren<NavigationListProps>, "ref"> & import("react").RefAttributes<HTMLUListElement>>;
-export {};
--- packages/ui/dist/src/components/Portal/index.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-interface PortalProps {
-    children?: React.ReactNode;
-    parent?: string;
-    className?: string;
-}
-export declare const Portal: React.ForwardRefExoticComponent<PortalProps & React.RefAttributes<HTMLDivElement>>;
-export {};
