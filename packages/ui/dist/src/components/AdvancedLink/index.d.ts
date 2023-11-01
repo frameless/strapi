0a1da32c3e8841aa61790a83b47feb201621d27c
Bryan de Jong <bryandejong99@gmail.com>
2023-11-01 06:41:55 +0000

    chore(vth): config views and labels

--- packages/ui/dist/src/components/AdvancedLink/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { Link as UtrechtLink } from '@utrecht/component-library-react/dist/css-module';
-import React, { ComponentType } from 'react';
-interface AdvancedLinkProps extends React.ComponentProps<typeof UtrechtLink> {
-    color?: 'red';
-    icon?: 'arrow' | 'chevronLeft';
-    Link?: ComponentType<any>;
-}
-export declare const AdvancedLink: React.ForwardRefExoticComponent<Omit<React.PropsWithChildren<AdvancedLinkProps>, "ref"> & React.RefAttributes<HTMLAnchorElement>>;
-export {};
