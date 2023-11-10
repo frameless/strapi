43e3b34d73d764184cb189c64139722fff042cde
Bryan de Jong <bryandejong99@gmail.com>
2023-11-10 00:47:49 +0000

    feat(vth): update query for hoofditem

--- apps/vth-frontend/src/app/[locale]/[hoofditemSlug]/page.tsx
+++ apps/vth-frontend/src/app/[locale]/[hoofditemSlug]/page.tsx
@@ -3,28 +3,25 @@ import { fetchData } from '@frameless/vth-frontend/src/util/fetchData';
 import { Metadata } from 'next';
 import React from 'react';
 import { AccordionProvider, Heading1, Markdown } from '@/components';
-import { BreadcrumbNavigationElement } from '@/components/BreadcrumbNavigation';
 import { BreadcrumbWithBacklink } from '@/components/BreadcrumbWithBacklink';
 import { Card } from '@/components/Card';
 import { Grid } from '@/components/Grid';
-import { LinkData, SideNavigation } from '@/components/SideNavigation';
-import { GET_THEMA_BY_SLUG } from '@/query';
-import { SiblingData } from '@/types';
+import { GET_HOOFDITEM_BY_SLUG } from '@/query';
 import { getImageBaseUrl } from '@/util/getImageBaseUrl';
 
 type Params = {
   params: {
     locale: string;
-    hoofdItemSlug: string;
+    hoofditemSlug: string;
   };
 };
 
-export async function generateMetadata({ params: { locale, hoofdItemSlug } }: Params): Promise<Metadata> {
+export async function generateMetadata({ params: { locale, hoofditemSlug } }: Params): Promise<Metadata> {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const { data } = await fetchData({
     url: createStrapiURL(),
-    query: GET_THEMA_BY_SLUG,
-    variables: { slug: hoofdItemSlug, locale: locale },
+    query: GET_HOOFDITEM_BY_SLUG,
+    variables: { slug: hoofditemSlug, locale: locale },
   });
   return {
     title: data.findSlug.data.attributes.title,
@@ -32,46 +29,14 @@ export async function generateMetadata({ params: { locale, hoofdItemSlug } }: Pa
   };
 }
 
-const Thema = async ({ params: { locale, hoofdItemSlug } }: Params) => {
+const Hoofditem = async ({ params: { locale, hoofditemSlug } }: Params) => {
   const { data } = await fetchData({
     url: createStrapiURL(),
-    query: GET_THEMA_BY_SLUG,
-    variables: { slug: hoofdItemSlug, locale: locale },
+    query: GET_HOOFDITEM_BY_SLUG,
+    variables: { slug: hoofditemSlug, locale: locale },
   });
 
-  const { title, content, parents, child_themas, child_contents } = data.findSlug.data.attributes;
-  const parentSlug = parents?.data[0]?.attributes?.slug;
-  const siblingThemas: SiblingData[] = parents?.data[0]?.attributes?.child_themas?.data || [];
-  const siblingContent: SiblingData[] = parents?.data[0]?.attributes?.child_contents?.data || [];
-
-  const themasLinks =
-    siblingThemas?.map(({ attributes: { slug, title } }: SiblingData) => ({
-      title,
-      slug,
-      href: `/themas/${slug}`,
-      isCurrent: slug === hoofdItemSlug,
-    })) || [];
-
-  const contentLinks =
-    siblingContent?.map(({ attributes: { slug, title } }: SiblingData) => ({
-      title,
-      slug,
-      href: `/themas/${parentSlug}/content/${slug}`,
-      isCurrent: slug === hoofdItemSlug,
-    })) || [];
-
-  const sideNavigationLinks: LinkData[] = [...themasLinks, ...contentLinks];
-
-  const breadcrumbNavigationElements: BreadcrumbNavigationElement[] = [];
-
-  const parentElement: BreadcrumbNavigationElement = parents?.data[0] && {
-    title: parents?.data[0]?.attributes?.title,
-    href: `/themas/${parentSlug}`,
-  };
-
-  if (parentElement) {
-    breadcrumbNavigationElements.push(parentElement);
-  }
+  const { title, content, themas, contents } = data.findSlug.data.attributes;
 
   const DynamicContent = () =>
     content &&
@@ -104,8 +69,8 @@ const Thema = async ({ params: { locale, hoofdItemSlug } }: Params) => {
     <Grid className={'utrecht-grid--content-padding'}>
       <div className={'utrecht-grid__full-width'}>
         <BreadcrumbWithBacklink
-          breadcrumbProps={{ navigationElements: breadcrumbNavigationElements }}
-          backlinkProps={{ title: parentElement?.title || 'Home', href: parentElement?.href || '/' }}
+          breadcrumbProps={{ navigationElements: [] }}
+          backlinkProps={{ title: 'Home', href: '/' }}
         />
       </div>
       <Grid className={'utrecht-grid__two-third'}>
@@ -114,8 +79,8 @@ const Thema = async ({ params: { locale, hoofdItemSlug } }: Params) => {
           <DynamicContent />
         </div>
         <Grid className={'utrecht-grid__full-width'}>
-          {child_themas.data[0] &&
-            child_themas.data.map((thema: any) => {
+          {themas.data[0] &&
+            themas.data.map((thema: any) => {
               const { title, description, slug: childSlug, previewImage: imageData } = thema.attributes;
               const imageUrl = imageData?.data?.attributes?.url;
               return (
@@ -125,13 +90,12 @@ const Thema = async ({ params: { locale, hoofdItemSlug } }: Params) => {
                   title={title}
                   description={description}
                   key={`thema-${childSlug}`}
-                  link={{ href: `/themas/${childSlug}` }}
+                  link={{ href: `${locale}/thema/${childSlug}` }}
                 />
               );
             })}
-          {child_contents.data[0] &&
-            child_contents.data &&
-            child_contents.data.map((content: any) => {
+          {contents.data[0] &&
+            contents.data.map((content: any) => {
               const { title, description, slug: contentSlug, previewImage: imageData } = content.attributes;
               const imageUrl = imageData?.data?.attributes?.url;
               return (
@@ -141,19 +105,14 @@ const Thema = async ({ params: { locale, hoofdItemSlug } }: Params) => {
                   description={description}
                   key={`thema-${contentSlug}`}
                   image={{ url: imageUrl && `${getImageBaseUrl()}${imageUrl}`, alt: '' }}
-                  link={{ href: `/themas/${hoofdItemSlug}/content/${contentSlug}` }}
+                  link={{ href: `${locale}/content/${contentSlug}` }}
                 />
               );
             })}
         </Grid>
       </Grid>
-      {sideNavigationLinks.length > 0 && (
-        <div className={'utrecht-grid-mobile-hidden utrecht-grid__one-third'}>
-          <SideNavigation links={sideNavigationLinks} />
-        </div>
-      )}
     </Grid>
   );
 };
 
-export default Thema;
+export default Hoofditem;
