8b2433f2baf070df2571103f18fd1b470e3585fe
Ali Amori Kadhim <ali@frameless.io>
2024-01-30 14:44:22 +0100

    chore(vth-frontend): use scss module instead of css for card

--- apps/vth-frontend/src/components/Card/index.tsx
+++ apps/vth-frontend/src/components/Card/index.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-import { Heading2, Paragraph } from '@utrecht/component-library-react';
-import clsx from 'clsx';
+import classnames from 'classnames/bind';
 import Image from 'next/image';
 import Link from 'next/link';
 import React, { FC, HTMLAttributes } from 'react';
-import './index.style.css';
+import { Heading2, Paragraph } from '@/components';
+import styles from './index.module.scss';
+
+const css = classnames.bind(styles);
 
 interface CardProps extends Omit<HTMLAttributes<HTMLDivElement>, 'children'> {
   title: string;
@@ -23,15 +25,15 @@ export const Card: FC<CardProps> = ({ description, title, image: { url = '', alt
   const linkRef = React.useRef<HTMLAnchorElement>(null);
 
   return (
-    <div {...props} className={clsx('utrecht-card', props.className)} onClick={() => linkRef.current?.click()}>
-      {url && <Image src={url} alt={alt} className={'utrecht-card__image'} width={312} height={200} />}
-      <div className={'utrecht-card__content'}>
-        <Heading2 className="utrecht-card__title">
-          <Link ref={linkRef} href={href} className={'utrecht-link utrecht-link--html-a'}>
+    <div {...props} className={css('utrecht-card', props.className)} onClick={() => linkRef.current?.click()}>
+      {url && <Image src={url} alt={alt} className={css('utrecht-card__image')} width={312} height={200} />}
+      <div className={css('utrecht-card__content')}>
+        <Heading2 className={css('utrecht-card__title')}>
+          <Link ref={linkRef} href={href} className={css('utrecht-link utrecht-link--html-a')}>
             {title}
           </Link>
         </Heading2>
-        <Paragraph className="utrecht-card__body">{description}</Paragraph>
+        <Paragraph className={css('utrecht-card__body')}>{description}</Paragraph>
       </div>
     </div>
   );
