f07ba848f3ff47d9bc898ae4aa1b77364039b4b0
Bryan de Jong <bryandejong99@gmail.com>
2024-01-30 08:19:09 +0000

    feat(pdc): add cimpdc productencatalogus

---
+++ apps/pdc-dashboard/src/api/productencatalogus/controllers/productencatalogus.ts
@@ -0,0 +1,7 @@
+/**
+ * productencatalogus controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::productencatalogus.productencatalogus');
---
+++ apps/pdc-dashboard/src/api/productencatalogus/routes/productencatalogus.ts
@@ -0,0 +1,7 @@
+/**
+ * productencatalogus router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::productencatalogus.productencatalogus');
