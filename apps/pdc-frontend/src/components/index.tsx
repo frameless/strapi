5ef6b97d40334bb04325fb89781b3ae954c33f65
Ali Amori Kadhim <ali@frameless.io>
2024-02-08 11:38:43 +0100

    chore: fix the breadcrumb warning issue

--- apps/pdc-frontend/src/components/index.tsx
+++ apps/pdc-frontend/src/components/index.tsx
@@ -1,6 +1,15 @@
 'use client';
 
+import dynamic from 'next/dynamic';
+
 export * from '@utrecht/web-component-library-react';
 export * from '@utrecht/component-library-react/dist/css-module';
 export * from '@frameless/ui';
 export { Markdown } from './Markdown';
+export const Breadcrumbs = dynamic(
+  async () => {
+    const Component = (await import('@frameless/ui')).Breadcrumbs;
+    return { default: Component };
+  },
+  { ssr: false },
+);
--- apps/vth-frontend/src/components/index.tsx
+++ apps/vth-frontend/src/components/index.tsx
@@ -1,6 +1,16 @@
 'use client';
 
+import dynamic from 'next/dynamic';
+
 export * from '@utrecht/component-library-react';
 export * from '@utrecht/web-component-library-react';
 export * from '@frameless/ui';
 export { Markdown } from './Markdown';
+
+export const Breadcrumbs = dynamic(
+  async () => {
+    const Component = (await import('@frameless/ui')).Breadcrumbs;
+    return { default: Component };
+  },
+  { ssr: false },
+);
