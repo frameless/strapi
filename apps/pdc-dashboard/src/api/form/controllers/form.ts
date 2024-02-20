5e002365fb474c8173c700721764d96a3ed8cb43
Ali Amori Kadhim <ali@frameless.io>
2024-02-20 12:59:45 +0100

    chore(pdc-dashboard): remove the form collection types

--- apps/pdc-dashboard/src/api/form/controllers/form.ts
+++
@@ -1,7 +0,0 @@
-/**
- * form controller
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreController('api::form.form');
--- apps/pdc-dashboard/src/api/form/services/form.ts
+++
@@ -1,7 +0,0 @@
-/**
- * form service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::form.form');
