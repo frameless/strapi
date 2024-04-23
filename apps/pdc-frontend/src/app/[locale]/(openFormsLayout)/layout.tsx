f5704ae32f0fcc7d5a58c76f7c28874fa579dd93
scar055 <rowanvanlaar@gmail.com>
2024-04-23 15:12:15 +0200

    fix: removes bold on the radio button label

--- apps/pdc-frontend/src/app/[locale]/(openFormsLayout)/layout.tsx
+++ apps/pdc-frontend/src/app/[locale]/(openFormsLayout)/layout.tsx
@@ -36,6 +36,7 @@ import { useTranslation } from '../../i18n/index';
 import { languages } from '../../i18n/settings';
 import '@frameless/ui/dist/bundle.css';
 import '../../../styles/globals.css';
+import '../../../styles/openforms.css';
 
 interface LayoutProps {
   children: React.ReactNode;
