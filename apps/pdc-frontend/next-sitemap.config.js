9928bcfab295ff99a73e4fac420e3a2b501ef813
Ali Amori Kadhim <ali@frameless.io>
2024-04-24 16:19:45 +0200

    chore(pdc-frontend): generate sitemap using Nextjs instead of next-sitemap packages

--- apps/pdc-frontend/next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: process.env.FRONTEND_PUBLIC_URL,
-  generateRobotsTxt: true,
-  exclude: ['/sitemap.xml'],
-  robotsTxtOptions: {
-    additionalSitemaps: [`${process.env.FRONTEND_PUBLIC_URL}/sitemap.xml`],
-  },
-};
---
+++ apps/pdc-frontend/src/app/[locale]/sitemap.ts
@@ -0,0 +1,30 @@
+import { MetadataRoute } from 'next';
+import { GET_ALL_PRODUCTS_SITEMAP } from '@/query';
+import { createStrapiURL } from '@/util/createStrapiURL';
+import { fetchData } from '@/util/fetchData';
+import { GetAllProductsSitemapQuery } from '../../../gql/graphql';
+
+const { origin } = new URL(process.env.FRONTEND_PUBLIC_URL || '');
+
+const generateStaticPagesPath = (paths: string[]) => {
+  return paths.map((path) => ({
+    url: `${origin}/${path}`,
+    lastModified: new Date().toISOString(),
+  }));
+};
+const staticPages = generateStaticPagesPath(['', 'products']);
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const { data } = await fetchData<{ data: GetAllProductsSitemapQuery }>({
+    url: createStrapiURL(),
+    query: GET_ALL_PRODUCTS_SITEMAP,
+  });
+
+  const products = data?.products?.data?.map((product) => ({
+    url: `${origin}/${product.attributes?.locale}/products/${product.attributes?.slug}`,
+    lastModified: product.attributes?.updatedAt,
+  }));
+
+  const fields = products?.concat(...staticPages);
+
+  return fields || staticPages;
+}
