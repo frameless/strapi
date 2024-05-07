940a22688846f02ddc6b331acfa9fe01aaeb96b4
Ali Amori Kadhim <ali@frameless.io>
2024-05-07 16:59:54 +0200

    feat(ui): handel the nav links focus
    This adjustment enables the Navigation to be focusable, allowing communication with the SkipLink component.

--- packages/ui/src/components/Navigation/NavigationList/index.tsx
+++ packages/ui/src/components/Navigation/NavigationList/index.tsx
@@ -1,5 +1,6 @@
 import classnames from 'classnames/bind';
-import { DetailedHTMLProps, ForwardedRef, forwardRef, HTMLAttributes, PropsWithChildren } from 'react';
+import { DetailedHTMLProps, forwardRef, HTMLAttributes, PropsWithChildren, useRef } from 'react';
+import { FocusEvent } from 'react';
 import styles from './index.module.scss';
 import { NavigationItem } from '../NavigationItem';
 import { NavigationLink } from '../NavigationLink';
@@ -14,34 +15,54 @@ interface NavigationListProps extends DetailedHTMLProps<HTMLAttributes<HTMLUList
 const css = classnames.bind(styles);
 
 export const NavigationList = forwardRef(
-  (
-    { list, mobile, sideNav, children, ...restProps }: PropsWithChildren<NavigationListProps>,
-    ref: ForwardedRef<HTMLUListElement>,
-  ) => (
-    <ul
-      className={css('utrecht-navigation__list', {
-        'utrecht-navigation__list--mobile': mobile,
-        'utrecht-navigation__list--side-nav': sideNav,
-      })}
-      ref={ref}
-      {...restProps}
-    >
-      {children}
+  ({ list, mobile, sideNav, children, ...restProps }: PropsWithChildren<NavigationListProps>) => {
+    const navListRef = useRef<HTMLUListElement>(null);
+    const navLinkRef = useRef<HTMLAnchorElement>(null);
 
-      {list &&
-        list.length > 0 &&
-        list.map((item, index) => (
-          <NavigationItem key={index} mobile={mobile}>
-            <NavigationLink mobile={mobile} href={item.href} isCurrent={item.isCurrent}>
-              {item.textContent}
-            </NavigationLink>
-            {mobile && item.children && item.children.length > 0 && (
-              <NavigationList list={item.children} mobile={mobile} />
-            )}
-          </NavigationItem>
-        ))}
-    </ul>
-  ),
+    // focus on first link in list when list is focused
+    const onNavListLinkFocusHandler = (event: FocusEvent<HTMLUListElement, Element>) => {
+      if (event.target !== navListRef.current) return; // ignore bubbling focus event in React
+
+      if (navListRef.current && navLinkRef?.current) {
+        navLinkRef.current.focus();
+      }
+    };
+
+    return (
+      <ul
+        className={css('utrecht-navigation__list', {
+          'utrecht-navigation__list--mobile': mobile,
+          'utrecht-navigation__list--side-nav': sideNav,
+        })}
+        ref={navListRef}
+        tabIndex={-1}
+        onFocus={onNavListLinkFocusHandler}
+        {...restProps}
+      >
+        {children}
+
+        {Array.isArray(list) &&
+          list.map((item, index) => {
+            const isTheFirstElement = index === 0;
+            return (
+              <NavigationItem key={index} mobile={mobile}>
+                <NavigationLink
+                  mobile={mobile}
+                  href={item.href}
+                  isCurrent={item.isCurrent}
+                  ref={isTheFirstElement ? navLinkRef : null}
+                >
+                  {item.textContent}
+                </NavigationLink>
+                {mobile && item.children && item.children.length > 0 && (
+                  <NavigationList list={item.children} mobile={mobile} />
+                )}
+              </NavigationItem>
+            );
+          })}
+      </ul>
+    );
+  },
 );
 
 NavigationList.displayName = 'NavigationList';
