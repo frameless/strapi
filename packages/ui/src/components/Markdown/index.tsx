74fd9f3b7e9c6ff64427c723734748a111ce89a6
MeesoPost <100311542+MeesoPost@users.noreply.github.com>
2024-05-13 15:04:56 +0200

    fix: add figure component to markdown rendering (#723)

--- packages/ui/src/components/Markdown/index.tsx
+++ packages/ui/src/components/Markdown/index.tsx
@@ -1,4 +1,4 @@
-import { OrderedList, TableContainer } from '@utrecht/component-library-react';
+import { Figure, OrderedList, TableContainer } from '@utrecht/component-library-react';
 import {
   Heading1,
   Heading2,
@@ -115,6 +115,10 @@ const defaultComponents = (config?: Components) => {
     caption: ({ children, node }) => {
       delete node.properties?.style;
       return <TableCaption {...node.properties}>{children}</TableCaption>;
+    },
+    figure: ({ children, node }) => {
+      delete node.properties?.style;
+      return <Figure {...node.properties}>{children}</Figure>;
     },
     figcaption: ({ children, node }) => {
       delete node.properties?.style;
