9e4d367658d949888b72cf3184324b69631dce96
Ali Amori Kadhim <ali@frameless.io>
2024-05-14 16:10:15 +0200

    feat(ui): ensure ScrollToTopButton forwards focus to specified element

--- packages/ui/src/components/ScrollToTopButton/index.tsx
+++ packages/ui/src/components/ScrollToTopButton/index.tsx
@@ -1,42 +1,29 @@
-import { Button } from '@utrecht/component-library-react';
+import { ButtonLink } from '@utrecht/component-library-react';
 import classnames from 'classnames/bind';
 import React, { ButtonHTMLAttributes, ForwardedRef, forwardRef, HTMLAttributes, PropsWithChildren } from 'react';
 import styles from './index.module.scss';
 
 const css = classnames.bind(styles);
 
-export interface ScrollToTopButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+export interface ScrollToTopButtonProps extends ButtonHTMLAttributes<HTMLAnchorElement> {
   Icon?: React.ComponentType<HTMLAttributes<HTMLElement>>;
 }
-
-export const scrollToTop = () => {
-  if (typeof window === 'undefined') return;
-
-  const reduceMotionQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
-  if (reduceMotionQuery.matches) {
-    // If prefers-reduced-motion is set to reduce, instantly jump to the top
-    window.scrollTo(0, 0);
-  } else {
-    // If not, use smooth scrolling animation
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  }
-};
 
 export const ScrollToTopButton = forwardRef(
   (
     { children, Icon, ...restProps }: PropsWithChildren<ScrollToTopButtonProps>,
-    ref: ForwardedRef<HTMLButtonElement>,
+    ref: ForwardedRef<HTMLAnchorElement>,
   ) => (
-    <Button
+    <ButtonLink
       className={css('utrecht-scroll-to-top-button')}
       appearance="subtle-button"
-      onClick={scrollToTop}
       ref={ref}
+      href="#top"
       {...restProps}
     >
       <span className={css('utrecht-scroll-to-top-button__content')}>{children}</span>
       {Icon && <Icon className={css('utrecht-scroll-to-top-button__icon')} />}
-    </Button>
+    </ButtonLink>
   ),
 );
 
