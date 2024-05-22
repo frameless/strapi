ca71dbfc126b2a3c0e4a9bd9c59249c22c438b1b
Ali Amori Kadhim <ali@frameless.io>
2024-05-22 10:41:57 +0200

    fix(ui): fix the Navigation focus trap

--- packages/ui/src/hooks/useClickOutside.ts
+++ packages/ui/src/hooks/useClickOutside.ts
@@ -1,31 +1,20 @@
 import { RefObject, useEffect } from 'react';
 
-export const useClickOutside = (
-  ref: RefObject<HTMLDialogElement>,
-  prevFocusableElement?: RefObject<HTMLButtonElement>,
-) => {
-  const handleClickOutside = (event: any) => {
-    if (ref.current && ref.current === event.target) {
-      ref.current.close();
-      if (prevFocusableElement) {
-        prevFocusableElement.current?.focus();
-      } else {
-        (document.activeElement as HTMLElement).focus();
-      }
+export const useClickOutside = (ref: RefObject<HTMLDialogElement>, callback?: () => void) => {
+  if (typeof document === 'undefined') return;
+  const handleClickOutside = (event: TouchEvent | MouseEvent) => {
+    if (ref.current && ref.current === event.target && ref.current.open && callback) {
+      callback();
     }
   };
 
   useEffect(() => {
-    const handleOutsideClick = (event: Event) => {
-      handleClickOutside(event);
-    };
-
-    document.addEventListener('mousedown', handleOutsideClick);
-    document.addEventListener('touchstart', handleOutsideClick);
+    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener('touchstart', handleClickOutside);
 
     return () => {
-      document.removeEventListener('mousedown', handleOutsideClick);
-      document.removeEventListener('touchstart', handleOutsideClick);
+      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener('touchstart', handleClickOutside);
     };
-  }, []);
+  }, [ref.current]);
 };
