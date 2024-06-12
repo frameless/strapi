63785171ed39739574a1be017916610669e60c61
Ali Amori Kadhim <ali@frameless.io>
2024-06-12 14:23:41 +0200

    feat(vth): add headingLevel property to the accordion

--- apps/vth-frontend/src/app/[locale]/article/[articleSlug]/page.tsx
+++ apps/vth-frontend/src/app/[locale]/article/[articleSlug]/page.tsx
@@ -127,10 +127,10 @@ const ArticlePage = async ({ params: { locale, articleSlug } }: Params) => {
         case 'ComponentComponentsUtrechtAccordion':
           return (
             <AccordionProvider
-              sections={component.item.map(({ id, label, body }: any) => ({
+              sections={component.item.map(({ id, label, body, headingLevel }: any) => ({
                 id,
                 label,
-                headingLevel: 3,
+                headingLevel,
                 body: <Markdown imageUrl={getImageBaseUrl()}>{body}</Markdown>,
               }))}
             />
