9f9caa4400648bfc350a4240e6e3d1041f31e306
Ali Amori Kadhim <ali@frameless.io>
2024-06-14 10:44:20 +0200

    feat(ui): add `NavSubList` component to the `NavigationList`

--- packages/ui/src/components/Navigation/NavigationList/index.tsx
+++ packages/ui/src/components/Navigation/NavigationList/index.tsx
@@ -4,18 +4,36 @@ import { FocusEvent } from 'react';
 import styles from './index.module.scss';
 import { NavigationItem } from '../NavigationItem';
 import { NavigationLink } from '../NavigationLink';
+import { NavigationMarker } from '../NavigationMarker';
 import { NavigationListType } from '../index';
-
-interface NavigationListProps extends DetailedHTMLProps<HTMLAttributes<HTMLUListElement>, HTMLUListElement> {
+export interface NavigationListProps extends DetailedHTMLProps<HTMLAttributes<HTMLUListElement>, HTMLUListElement> {
   list: NavigationListType[];
   mobile?: boolean;
   sideNav?: boolean;
+  subList?: boolean;
 }
 
 const css = classnames.bind(styles);
 
+const NavSubList = ({ list }: { list: NavigationListType[] }) => (
+  <ul className={css('utrecht-navigation__list--sub-list')}>
+    {list.map((item, index) => (
+      <NavigationItem key={index} mobile className={css('utrecht-navigation__list--sub-list-item')}>
+        <NavigationLink
+          href={item.href}
+          mobile
+          marker={<NavigationMarker isCurrent={item.isCurrent} appearance="outline" />}
+        >
+          {item.textContent}
+        </NavigationLink>
+        {item.children && Array.isArray(item.children) && <NavSubList list={item.children} />}
+      </NavigationItem>
+    ))}
+  </ul>
+);
+
 export const NavigationList = forwardRef(
-  ({ list, mobile, sideNav, children, ...restProps }: PropsWithChildren<NavigationListProps>) => {
+  ({ list, mobile, sideNav, children, subList, ...restProps }: PropsWithChildren<NavigationListProps>) => {
     const navListRef = useRef<HTMLUListElement>(null);
     const navLinkRef = useRef<HTMLAnchorElement>(null);
 
@@ -33,6 +51,7 @@ export const NavigationList = forwardRef(
         className={css('utrecht-navigation__list', {
           'utrecht-navigation__list--mobile': mobile,
           'utrecht-navigation__list--side-nav': sideNav,
+          'utrecht-navigation__list--sub-list': subList,
         })}
         ref={navListRef}
         tabIndex={-1}
@@ -51,12 +70,11 @@ export const NavigationList = forwardRef(
                   href={item.href}
                   isCurrent={item.isCurrent}
                   ref={isTheFirstElement ? navLinkRef : null}
+                  marker={mobile && <NavigationMarker isCurrent={item.isCurrent} />}
                 >
                   {item.textContent}
                 </NavigationLink>
-                {mobile && item.children && item.children.length > 0 && (
-                  <NavigationList list={item.children} mobile={mobile} />
-                )}
+                {mobile && item.children && item.children.length > 0 && <NavSubList list={item.children} />}
               </NavigationItem>
             );
           })}
