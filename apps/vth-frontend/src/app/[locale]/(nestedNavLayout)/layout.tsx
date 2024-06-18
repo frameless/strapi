4ddc03deb6d543e78b9fa25687dee38aaaf54fb4
Ali Amori Kadhim <ali@frameless.io>
2024-06-18 14:06:00 +0200

    chore(vth-frontend): integrate `socialMediaAriaLabel` into Footer component

--- apps/vth-frontend/src/app/[locale]/(nestedNavLayout)/layout.tsx
+++ apps/vth-frontend/src/app/[locale]/(nestedNavLayout)/layout.tsx
@@ -128,7 +128,14 @@ const RootLayout = async ({ children, params: { locale } }: LayoutProps) => {
               {children}
             </Page>
           </Surface>
-          <Footer data={footerData as FooterData} />
+          <Footer
+            data={footerData as FooterData}
+            label={
+              t('footer.socialMediaList.label', {
+                defaultValue: 'Social media links',
+              }) as string
+            }
+          />
         </QueryClientProvider>
         <Script async src="https://siteimproveanalytics.com/js/siteanalyze_6006206.js"></Script>
       </body>
