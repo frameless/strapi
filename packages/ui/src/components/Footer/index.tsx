6d016a22c41c97197246007e6a52490193df1311
Ali Amori Kadhim <ali@frameless.io>
2024-06-18 13:57:51 +0200

    feat(ui): add `socialMediaAriaLabel` to the footer component

--- packages/ui/src/components/Footer/index.tsx
+++ packages/ui/src/components/Footer/index.tsx
@@ -1,4 +1,4 @@
-import { Heading2, Heading3, Link, Page, PageFooter } from '@utrecht/component-library-react';
+import { Heading2, Heading3, LinkList, LinkListLink, Page, PageFooter } from '@utrecht/component-library-react';
 import {
   UtrechtIconChevronRight,
   UtrechtIconFacebook,
@@ -12,6 +12,7 @@ import classnames from 'classnames/bind';
 import styles from './index.module.scss';
 import { Grid, GridCell } from '../Grid';
 import { Markdown } from '../Markdown';
+import { Nav, type NavProps } from '../Nav';
 
 const css = classnames.bind(styles);
 
@@ -48,79 +49,72 @@ const socialMediaIconTypes = {
   whatsapp: UtrechtIconWhatsapp,
   newsletter: UtrechtIconNieuwsbrief,
 };
-
-export interface FooterProps {
+export interface FooterProps extends NavProps {
   data: FooterData;
 }
 
-export const Footer = ({ data }: FooterProps) => (
-  <PageFooter className={css('utrecht-footer')}>
-    <Page>
-      <Grid>
-        <GridCell md={8} sm={10}>
-          <Grid>
-            <GridCell md={12}>{data?.title && <Heading2>{data?.title}</Heading2>}</GridCell>
-            <GridCell md={6} sm={6}>
-              <ul className={css('utrecht-link-list', 'utrecht-link-list--html-ul')}>
-                {data?.list &&
-                  data.list?.listItem?.length > 0 &&
-                  data?.list?.listItem?.map((item, index) => (
-                    <li key={index} className={css('utrecht-link-list__item')}>
-                      {item?.title && <Heading3 className={css('utrecht-footer__list-title')}>{item?.title}</Heading3>}
-                      <Grid flexDirection="column" spacing="sm">
-                        {item?.link &&
-                          item?.link?.length > 0 &&
-                          item?.link?.map((item, index) => {
-                            const isPhoneNumber = item.href.includes('tel:14030');
-                            return (
-                              <GridCell key={index} sm={12}>
-                                <Link
-                                  key={index}
-                                  external
-                                  href={item.href}
-                                  className={css('utrecht-link-list__link', {
-                                    'utrecht-link-list__link--phone': isPhoneNumber,
-                                  })}
-                                >
-                                  {!isPhoneNumber && <UtrechtIconChevronRight />}
-                                  <span className="utrecht-link-list__link-text">{item.textContent}</span>
-                                </Link>
-                              </GridCell>
-                            );
-                          })}
-                      </Grid>
-                    </li>
-                  ))}
-              </ul>
-            </GridCell>
-            <GridCell md={6} sm={6}>
-              {data?.address && <Markdown>{data?.address}</Markdown>}
-            </GridCell>
-          </Grid>
-        </GridCell>
-        <GridCell md={4} sm={2} className={css('utrecht-grid--justify-content-sm-flex-end')}>
-          <ul className={css('utrecht-link-list', 'utrecht-link-list--html-ul', 'utrecht-link-list--social-media')}>
-            {data?.socialMediaList &&
-              data.socialMediaList?.link.length > 0 &&
-              data.socialMediaList?.link.map((item, index) => {
-                const Icon = socialMediaIconTypes[item.icon as keyof typeof socialMediaIconTypes];
-                return (
-                  <li key={index} className={css('utrecht-link-list__item')}>
-                    <Link
-                      external
-                      href={item.href}
-                      className={css('utrecht-link-list__link')}
-                      aria-label={item.textContent}
-                    >
-                      <Icon />
-                      <span className="utrecht-link-list__link-text">{item.textContent}</span>
-                    </Link>
-                  </li>
-                );
-              })}
-          </ul>
-        </GridCell>
-      </Grid>
-    </Page>
-  </PageFooter>
-);
+export const Footer = ({ data, headingLevel, label }: FooterProps) => {
+  const links = data?.socialMediaList?.link?.map((item) => {
+    const Icon = socialMediaIconTypes[item.icon as keyof typeof socialMediaIconTypes];
+    return {
+      ...item,
+      children: item.textContent,
+      icon: <Icon role="presentation" />,
+    };
+  });
+
+  return (
+    <PageFooter className={css('utrecht-footer')}>
+      <Page>
+        <Grid>
+          <GridCell md={8} sm={10}>
+            <Grid>
+              <GridCell md={12}>{data?.title && <Heading2>{data?.title}</Heading2>}</GridCell>
+              <GridCell md={6} sm={6}>
+                <ul className={css('utrecht-link-list', 'utrecht-link-list--html-ul')}>
+                  {data?.list &&
+                    data.list?.listItem?.length > 0 &&
+                    data?.list?.listItem?.map((item, index) => (
+                      <li key={index} className={css('utrecht-link-list__item')}>
+                        {item?.title && (
+                          <Heading3 className={css('utrecht-footer__list-title')}>{item?.title}</Heading3>
+                        )}
+                        <Grid flexDirection="column" spacing="sm">
+                          {item?.link &&
+                            item?.link?.length > 0 &&
+                            item?.link?.map((item, index) => {
+                              const isPhoneNumber = item.href.includes('tel:14030');
+                              return (
+                                <GridCell key={index} sm={12}>
+                                  <LinkListLink
+                                    icon={!isPhoneNumber && <UtrechtIconChevronRight />}
+                                    className={css('utrecht-link-list__link', {
+                                      'utrecht-link-list__link--phone': isPhoneNumber,
+                                    })}
+                                    href={item.href}
+                                  >
+                                    {item.textContent}
+                                  </LinkListLink>
+                                </GridCell>
+                              );
+                            })}
+                        </Grid>
+                      </li>
+                    ))}
+                </ul>
+              </GridCell>
+              <GridCell md={6} sm={6}>
+                {data?.address && <Markdown>{data?.address}</Markdown>}
+              </GridCell>
+            </Grid>
+          </GridCell>
+          <GridCell md={4} sm={2} className={css('utrecht-grid--justify-content-sm-flex-end')}>
+            <Nav headingLevel={headingLevel} label={label}>
+              <LinkList links={links} className={css('utrecht-footer__link-social-media-list')} />
+            </Nav>
+          </GridCell>
+        </Grid>
+      </Page>
+    </PageFooter>
+  );
+};
