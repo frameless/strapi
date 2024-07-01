c03733e357fea0c617ada633965e82657dd81fe3
Ali Amori Kadhim <ali@frameless.io>
2024-07-01 14:22:23 +0200

    fixup! wip: add app for tiptap

--- apps/tiptap-sandbox/vite.config.ts
+++ apps/tiptap-sandbox/vite.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import vitePluginRequire from 'vite-plugin-require';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-expect-error
   plugins: [vitePluginRequire.default(), react()],
   build: {
