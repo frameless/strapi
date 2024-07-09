abd85130b045f04bbd74aa03ca373bf0178395b9
Robbert Broersma <git@robbertbroersma.nl>
2024-07-10 00:02:25 +0200

    fix: show Heading 1 with form name while loading form

---
+++ apps/pdc-frontend/src/components/OpenFormsEmbed/OpenFormsScript.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import Script from 'next/script';
+import React, { useRef } from 'react';
+
+export type OpenFormsScriptProps = {
+  nonce: string;
+  src: string;
+  targetId: string;
+};
+
+interface OpenForm {
+  new (_targetNode: Node, _opts: Object): OpenForm;
+
+  init(): Promise<void>;
+}
+
+declare global {
+  // eslint-disable-next-line no-unused-vars
+  interface Window {
+    OpenForms: {
+      OpenForm: OpenForm;
+    };
+  }
+}
+
+export const OpenFormsScript = ({ targetId, nonce, src }: OpenFormsScriptProps) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const onLoadOpenForms = () => {
+    if (!ref.current) {
+      return;
+    }
+
+    // @ts-ignore
+    const target = ref.current.ownerDocument.getElementById(targetId);
+
+    if (target && window.OpenForms) {
+      const form = new window.OpenForms.OpenForm(target, target.dataset);
+      form.init();
+    }
+  };
+
+  return (
+    <>
+      <div ref={ref}></div>
+      <Script nonce={nonce} strategy="afterInteractive" src={src} onLoad={onLoadOpenForms} />
+    </>
+  );
+};
