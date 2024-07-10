ad2d6dbe45c44651e460f7e59ced376df8cac274
Robbert Broersma <git@robbertbroersma.nl>
2024-07-10 17:26:46 +0200

    style: layout improvements for audit rapport

--- apps/pdc-frontend/src/components/AuditRapport/index.tsx
+++ apps/pdc-frontend/src/components/AuditRapport/index.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-head-element */
 import {
+  Article,
   DataList,
   DataListItem,
   DataListKey,
@@ -87,10 +88,8 @@ export const AuditRapport = ({ evaluation }: { evaluation: WcagEmJson }) => {
 
   return (
     <>
-      {/* <CodeBlock>{JSON.stringify(badDeveloperResults, null, 2)}</CodeBlock> */}
-      <div>
-        {' '}
-        <main>
+      <main>
+        <Article>
           <Heading1>Toegankelijkheidsevaluatierapport</Heading1>
           <div>
             <Heading2>Over de Evaluatie</Heading2>
@@ -105,17 +104,22 @@ export const AuditRapport = ({ evaluation }: { evaluation: WcagEmJson }) => {
               </DataListItem>
               <DataListItem>
                 <DataListKey>Evaluatiedatum</DataListKey>
-                <DataListValue>{evaluation.reportFindings.date['@value']}</DataListValue>
+                <DataListValue>
+                  {new Intl.DateTimeFormat('nl-NL', {
+                    dateStyle: 'full',
+                    timeZone: 'Europe/Amsterdam',
+                  }).format(new Date(evaluation.reportFindings.date['@value']))}
+                </DataListValue>
               </DataListItem>
             </DataList>
           </div>
-          <div>
+          <section>
             <Heading2>Samenvatting</Heading2>
             <div>
               <Paragraph>{evaluation.reportFindings.summary}</Paragraph>
             </div>
-          </div>
-          <div>
+          </section>
+          <section>
             <Heading2>Scope of the Evaluation</Heading2>
             <DataList>
               <DataListItem>
@@ -143,110 +147,123 @@ export const AuditRapport = ({ evaluation }: { evaluation: WcagEmJson }) => {
                 <DataListValue>{evaluation.defineScope.additionalEvaluationRequirements}</DataListValue>
               </DataListItem>
             </DataList>
-          </div>
-          <Heading2>Detailed Audit Results</Heading2>
-          <Heading3>Samenvatting</Heading3>
-          <Paragraph>Reported on {totalSummary} of 55 WCAG 2.2 AA Success Criteria.</Paragraph>
-          <UnorderedList>
-            <UnorderedListItem>
-              <span>{outcomeCounts.Passed}</span> <span>geslaagd</span>
-            </UnorderedListItem>
-            <UnorderedListItem>
-              <span>{outcomeCounts.Failed}</span> <span>gefaald</span>
-            </UnorderedListItem>
-            <UnorderedListItem>
-              <span>{outcomeCounts['Cannot tell']}</span> <span>geen uitsluitsel</span>
-            </UnorderedListItem>
-            <UnorderedListItem>
-              <span>{outcomeCounts['Not present']}</span> <span>niet aangetroffen</span>
-            </UnorderedListItem>
-            <UnorderedListItem>
-              <span>{outcomeCounts['Not checked']}</span> <span>niet onderzocht</span>
-            </UnorderedListItem>
-          </UnorderedList>
-          <Heading3>All Results</Heading3>
-          {successCriteria
-            .map((sc) => ({
-              ...sc,
-              fragment: new URL(sc.url).hash.replace(/^#/, ''),
-            }))
-            .map(({ sc, fragment, title, url }) => (
-              <section id={fragment} key={sc}>
-                <Heading4>
-                  {sc} {title}
-                </Heading4>
-                {Array.isArray(badDeveloperResults[url]) && badDeveloperResults[url].length > 0 ? (
-                  badDeveloperResults[url].length > 1 ? (
-                    <div>
-                      <Paragraph>Gevonden issues:</Paragraph>
-                      <UnorderedList>
-                        {badDeveloperResults[url].map((auditSample, index) => (
-                          <UnorderedListItem key={index}>
-                            <Paragraph>
-                              {
-                                titleMapping[
-                                  badDeveloperResults[url][0].result.outcome.title as keyof typeof titleMapping
-                                ]
-                              }
-                            </Paragraph>
-                            <Markdown>{auditSample.result.description}</Markdown>
-                          </UnorderedListItem>
-                        ))}
-                      </UnorderedList>
-                    </div>
-                  ) : (
-                    <div>
-                      <Paragraph>
-                        <strong>
-                          {titleMapping[badDeveloperResults[url][0].result.outcome.title as keyof typeof titleMapping]}.
-                        </strong>
-                      </Paragraph>
-
-                      {badDeveloperResults[url][0].result.outcome.title === 'Failed' ? (
+          </section>
+          <section>
+            <Heading2>Detailed Audit Results</Heading2>
+            <section>
+              <Heading3>Samenvatting</Heading3>
+              <Paragraph>Reported on {totalSummary} of 55 WCAG 2.2 AA Success Criteria.</Paragraph>
+              <UnorderedList>
+                <UnorderedListItem>
+                  <span>{outcomeCounts.Passed}</span> <span>geslaagd</span>
+                </UnorderedListItem>
+                <UnorderedListItem>
+                  <span>{outcomeCounts.Failed}</span> <span>gefaald</span>
+                </UnorderedListItem>
+                <UnorderedListItem>
+                  <span>{outcomeCounts['Cannot tell']}</span> <span>geen uitsluitsel</span>
+                </UnorderedListItem>
+                <UnorderedListItem>
+                  <span>{outcomeCounts['Not present']}</span> <span>niet aangetroffen</span>
+                </UnorderedListItem>
+                <UnorderedListItem>
+                  <span>{outcomeCounts['Not checked']}</span> <span>niet onderzocht</span>
+                </UnorderedListItem>
+              </UnorderedList>
+              <Heading3>All Results</Heading3>
+              {successCriteria
+                .map((sc) => ({
+                  ...sc,
+                  fragment: new URL(sc.url).hash.replace(/^#/, ''),
+                }))
+                .map(({ sc, fragment, nl: { title }, url, nldesignsystem }) => (
+                  <section id={fragment} key={sc}>
+                    <Heading4>
+                      {sc} {title}
+                    </Heading4>
+                    {Array.isArray(badDeveloperResults[url]) && badDeveloperResults[url].length > 0 ? (
+                      badDeveloperResults[url].length > 1 ? (
                         <div>
-                          <Paragraph>Gevonden issue:</Paragraph>
-                          <Markdown>{badDeveloperResults[url][0].result.description}</Markdown>
+                          <Paragraph>Gevonden issues:</Paragraph>
+                          <UnorderedList>
+                            {badDeveloperResults[url].map((auditSample, index) => (
+                              <UnorderedListItem key={index}>
+                                <Paragraph>
+                                  {
+                                    titleMapping[
+                                      badDeveloperResults[url][0].result.outcome.title as keyof typeof titleMapping
+                                    ]
+                                  }
+                                </Paragraph>
+                                <Markdown>{auditSample.result.description}</Markdown>
+                              </UnorderedListItem>
+                            ))}
+                          </UnorderedList>
                         </div>
-                      ) : null}
-                    </div>
-                  )
-                ) : (
-                  <Paragraph>
-                    <strong>Geen problemen vastgesteld.</strong>
-                  </Paragraph>
-                )}
+                      ) : (
+                        <div>
+                          <Paragraph>
+                            {
+                              titleMapping[
+                                badDeveloperResults[url][0].result.outcome.title as keyof typeof titleMapping
+                              ]
+                            }
+                            .
+                          </Paragraph>
 
-                <Paragraph>
-                  Lees meer bij NL Design System:&nbsp;
-                  <Link href={`https://nldesignsystem.nl/wcag/${sc}`} external>
-                    WCAG 2.2 {sc} {title}
+                          {badDeveloperResults[url][0].result.outcome.title === 'Failed' ? (
+                            <div>
+                              <Paragraph>Gevonden issue:</Paragraph>
+                              <Markdown>{badDeveloperResults[url][0].result.description}</Markdown>
+                            </div>
+                          ) : null}
+                        </div>
+                      )
+                    ) : (
+                      <Paragraph>Geen problemen vastgesteld.</Paragraph>
+                    )}
+
+                    {nldesignsystem && (
+                      <Paragraph>
+                        Lees meer bij NL Design System:&nbsp;
+                        <Link href={`https://nldesignsystem.nl/wcag/${sc}`} external>
+                          WCAG-succescriteria uitgelegd: {title}
+                        </Link>
+                      </Paragraph>
+                    )}
+                  </section>
+                ))}
+            </section>
+          </section>
+
+          <section>
+            <Heading2>Deze pagina&apos;s zijn onderzocht:</Heading2>
+            <OrderedList>
+              {evaluation.selectSample.structuredSample.map(({ description, id, title }) => (
+                <OrderedListItem key={id}>
+                  <Link href={description} external>
+                    {title}
                   </Link>
-                </Paragraph>
-              </section>
-            ))}
+                </OrderedListItem>
+              ))}
+            </OrderedList>
+          </section>
 
-          <Heading2>Deze pagina&apos;s zijn onderzocht:</Heading2>
-          <OrderedList>
-            {evaluation.selectSample.structuredSample.map(({ description, id, title }) => (
-              <OrderedListItem key={id}>
-                <Link href={description} external>
-                  {title}
-                </Link>
-              </OrderedListItem>
-            ))}
-          </OrderedList>
+          <section>
+            <Heading2>Gebruikte technologie</Heading2>
+            <UnorderedList>
+              {evaluation.exploreTarget.technologiesReliedUpon.map((i) => (
+                <UnorderedListItem key={i}>{i}</UnorderedListItem>
+              ))}
+            </UnorderedList>
+          </section>
 
-          <Heading2>Web Technologie</Heading2>
-          <UnorderedList>
-            {evaluation.exploreTarget.technologiesReliedUpon.map((i) => (
-              <UnorderedListItem key={i}>{i}</UnorderedListItem>
-            ))}
-          </UnorderedList>
-
-          <Heading2>Bijzonderheden</Heading2>
-          <Paragraph>{evaluation.reportFindings.evaluationSpecifics}</Paragraph>
-        </main>
-      </div>
+          <section>
+            <Heading2>Bijzonderheden</Heading2>
+            <Paragraph>{evaluation.reportFindings.evaluationSpecifics}</Paragraph>
+          </section>
+        </Article>
+      </main>
     </>
   );
 };
