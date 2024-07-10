8b75823ab410c76e86f0cb0493eed5d2cf8daf54
Robbert Broersma <git@robbertbroersma.nl>
2024-07-10 22:23:10 +0200

    docs: custom WCAG descriptions

---
+++ apps/pdc-frontend/src/components/AuditRapport/_wcag-1.2.6-intro-frameless.md
@@ -0,0 +1,3 @@
+<!-- @license CC0-1.0 -->
+
+Bij alle audio en video is ook een video met gebarentaal beschikbaar.
--- apps/pdc-frontend/src/components/AuditRapport/intro.ts
+++ apps/pdc-frontend/src/components/AuditRapport/intro.ts
@@ -1,18 +1,51 @@
 import wcag111intro from './_wcag-1.1.1-intro.md';
+import wcag121intro from './_wcag-1.2.1-intro-frameless.md';
+import wcag122intro from './_wcag-1.2.2-intro-frameless.md';
+import wcag123intro from './_wcag-1.2.3-intro-frameless.md';
+import wcag124intro from './_wcag-1.2.4-intro-frameless.md';
+import wcag125intro from './_wcag-1.2.5-intro-frameless.md';
+import wcag126intro from './_wcag-1.2.6-intro-frameless.md';
 import wcag131intro from './_wcag-1.3.1-intro.md';
+import wcag132intro from './_wcag-1.3.2-intro-frameless.md';
+import wcag133intro from './_wcag-1.3.3-intro-frameless.md';
+import wcag134intro from './_wcag-1.3.4-intro-frameless.md';
 import wcag135intro from './_wcag-1.3.5-intro.md';
+import wcag141intro from './_wcag-1.4.1-intro-frameless.md';
+import wcag1410intro from './_wcag-1.4.10-intro-frameless.md';
+import wcag1411intro from './_wcag-1.4.11-intro-frameless.md';
+import wcag1412intro from './_wcag-1.4.12-intro-frameless.md';
+import wcag1413intro from './_wcag-1.4.13-intro-frameless.md';
+import wcag142intro from './_wcag-1.4.2-intro-frameless.md';
+import wcag143intro from './_wcag-1.4.3-intro-frameless.md';
+import wcag144intro from './_wcag-1.4.4-intro-frameless.md';
+import wcag145intro from './_wcag-1.4.5-intro-frameless.md';
 import wcag211intro from './_wcag-2.1.1-intro.md';
+import wcag212intro from './_wcag-2.1.2-intro-frameless.md';
+import wcag214intro from './_wcag-2.1.4-intro-frameless.md';
+import wcag221intro from './_wcag-2.2.1-intro-frameless.md';
+import wcag222intro from './_wcag-2.2.2-intro-frameless.md';
+import wcag231intro from './_wcag-2.3.1-intro-frameless.md';
 import wcag241intro from './_wcag-2.4.1-intro.md';
 import wcag2410intro from './_wcag-2.4.10-intro.md';
 import wcag2413intro from './_wcag-2.4.13-intro.md';
 import wcag242intro from './_wcag-2.4.2-intro.md';
+import wcag243intro from './_wcag-2.4.3-intro-frameless.md';
 import wcag244intro from './_wcag-2.4.4-intro.md';
+import wcag245intro from './_wcag-2.4.5-intro-frameless.md';
 import wcag246intro from './_wcag-2.4.6-intro.md';
 import wcag247intro from './_wcag-2.4.7-intro.md';
+import wcag251intro from './_wcag-2.5.1-intro-frameless.md';
+import wcag252intro from './_wcag-2.5.2-intro-frameless.md';
+import wcag253intro from './_wcag-2.5.3-intro-frameless.md';
+import wcag254intro from './_wcag-2.5.4-intro-frameless.md';
 import wcag311intro from './_wcag-3.1.1-intro.md';
 import wcag312intro from './_wcag-3.1.2-intro.md';
+import wcag321intro from './_wcag-3.2.1-intro-frameless.md';
 import wcag322intro from './_wcag-3.2.2-intro.md';
+import wcag323intro from './_wcag-3.2.3-intro-frameless.md';
+import wcag324intro from './_wcag-3.2.4-intro-frameless.md';
 import wcag331intro from './_wcag-3.3.1-intro.md';
+import wcag332intro from './_wcag-3.3.2-intro-frameless.md';
 import wcag333intro from './_wcag-3.3.3-intro.md';
 import wcag334intro from './_wcag-3.3.4-intro.md';
 import wcag411intro from './_wcag-4.1.1-intro.md';
@@ -21,20 +54,53 @@ import wcag413intro from './_wcag-4.1.3-intro.md';
 
 export const wcagIntro: { [index: string]: string } = {
   '1.1.1': wcag111intro as any as string,
+  '1.2.1': wcag121intro as any as string,
+  '1.2.2': wcag122intro as any as string,
+  '1.2.3': wcag123intro as any as string,
+  '1.2.4': wcag124intro as any as string,
+  '1.2.5': wcag125intro as any as string,
+  '1.2.6': wcag126intro as any as string,
   '1.3.1': wcag131intro as any as string,
+  '1.3.2': wcag132intro as any as string,
+  '1.3.3': wcag133intro as any as string,
+  '1.3.4': wcag134intro as any as string,
   '1.3.5': wcag135intro as any as string,
+  '1.4.1': wcag141intro as any as string,
+  '1.4.2': wcag142intro as any as string,
+  '1.4.3': wcag143intro as any as string,
+  '1.4.4': wcag144intro as any as string,
+  '1.4.5': wcag145intro as any as string,
+  '1.4.10': wcag1410intro as any as string,
+  '1.4.11': wcag1411intro as any as string,
+  '1.4.12': wcag1412intro as any as string,
+  '1.4.13': wcag1413intro as any as string,
   '2.1.1': wcag211intro as any as string,
+  '2.1.2': wcag212intro as any as string,
+  '2.1.4': wcag214intro as any as string,
+  '2.2.1': wcag221intro as any as string,
+  '2.2.2': wcag222intro as any as string,
+  '2.3.1': wcag231intro as any as string,
   '2.4.1': wcag241intro as any as string,
   '2.4.13': wcag2413intro as any as string,
   '2.4.2': wcag242intro as any as string,
+  '2.4.3': wcag243intro as any as string,
   '2.4.4': wcag244intro as any as string,
+  '2.4.5': wcag245intro as any as string,
   '2.4.6': wcag246intro as any as string,
   '2.4.7': wcag247intro as any as string,
   '2.4.10': wcag2410intro as any as string,
+  '2.5.1': wcag251intro as any as string,
+  '2.5.2': wcag252intro as any as string,
+  '2.5.3': wcag253intro as any as string,
+  '2.5.4': wcag254intro as any as string,
   '3.1.1': wcag311intro as any as string,
   '3.1.2': wcag312intro as any as string,
   '3.2.2': wcag322intro as any as string,
+  '3.2.1': wcag321intro as any as string,
+  '3.2.3': wcag323intro as any as string,
+  '3.2.4': wcag324intro as any as string,
   '3.3.1': wcag331intro as any as string,
+  '3.3.2': wcag332intro as any as string,
   '3.3.3': wcag333intro as any as string,
   '3.3.4': wcag334intro as any as string,
   '4.1.1': wcag411intro as any as string,
