a9683ce84b0c2e3d237d6cd70a117f47538d0787
Robbert Broersma <git@robbertbroersma.nl>
2024-07-10 10:05:35 +0200

    feat: create Docusaurus documentation website for Strapi

---
+++ apps/strapi.frameless.io/docusaurus.config.ts
@@ -0,0 +1,144 @@
+import type * as Preset from '@docusaurus/preset-classic';
+import type { Config } from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+
+const config: Config = {
+  title: 'Strapi van Frameless',
+  tagline: 'Digitaal toegankelijke websites en een handig CMS.',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://strapi.frameless.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'frameless', // Usually your GitHub org/user name.
+  projectName: 'strapi', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'nl',
+    locales: ['nl'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    colorMode: {
+      respectPrefersColorScheme: false,
+      disableSwitch: true,
+    },
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'Strapi van Frameless',
+      logo: {
+        alt: 'Logo van Frameless',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Handboek',
+        },
+        {
+          type: 'docSidebar',
+          sidebarId: 'developersSidebar',
+          position: 'left',
+          label: 'Developers',
+        },
+        { to: '/blog', label: 'Blog', position: 'left' },
+        {
+          href: 'https://github.com/frameless/strapi',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      links: [
+        {
+          title: 'Documentatie',
+          items: [
+            {
+              label: 'Handboek',
+              to: '/docs/handboek/intro',
+            },
+            {
+              label: 'Open source licentie',
+              to: '/docs/developers/license',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'LinkedIn',
+              href: 'http://linkedin.com/company/frameless-io/',
+            },
+            {
+              label: 'GitHub Issues',
+              href: 'https://github.com/frameless/strapi/issues',
+            },
+            {
+              label: 'Common Ground',
+              href: 'https://commonground.nl/',
+            },
+          ],
+        },
+        {
+          title: 'Meer',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/frameless/strapi',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Frameless B.V.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
