fa93654ba787719cefe31c0de0362a53da852be9
Robbert Broersma <git@robbertbroersma.nl>
2024-07-11 04:26:12 +0200

    build: configure markdown imports for Next.js

--- apps/pdc-frontend/next.config.js
+++ apps/pdc-frontend/next.config.js
@@ -11,6 +11,15 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.md$/,
+      // This is the asset module.
+      type: 'asset/source',
+    });
+    return config;
+  },
+
   experimental: {
     serverActions: true,
   },
