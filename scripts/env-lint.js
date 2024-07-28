3429b56df1592ea1578a4f723a78d8a20b203106
Robbert Broersma <git@robbertbroersma.nl>
2024-07-28 12:09:13 +0200

    ci: do not require .env file in CI

--- scripts/env-lint.js
+++ scripts/env-lint.js
@@ -2,7 +2,18 @@ const { config } = require('dotenv');
 const { validateEnv } = require('./env-lib');
 const { spec } = require('../.envrc.json');
 
+const CI = process.env['CI'] === 'true';
+
 const { parsed: env = {} } = config();
+
+if (CI) {
+  if (Object.keys(env).length > 0) {
+    process.stderr.write('In CI the .env file must not be present.\n');
+    process.exit(1);
+  } else {
+    process.exit(0);
+  }
+}
 
 const allErrors = validateEnv(env, spec);
 
