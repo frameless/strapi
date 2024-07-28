437647c74d0961f1e7ef2deb5a53dbfdcf7feeb8
Robbert Broersma <git@robbertbroersma.nl>
2024-07-28 16:49:27 +0200

    fix: avoid ForwardRef warnings in the console

--- packages/ui/src/components/Navigation/NavigationMarker/index.tsx
+++ packages/ui/src/components/Navigation/NavigationMarker/index.tsx
@@ -1,5 +1,5 @@
 import classnames from 'classnames/bind';
-import { forwardRef, PropsWithChildren, SVGProps } from 'react';
+import { PropsWithChildren, SVGProps } from 'react';
 import styles from './index.module.scss';
 
 const css = classnames.bind(styles);
@@ -9,48 +9,46 @@ interface NavigationMarkerProps extends SVGProps<SVGSVGElement> {
   appearance?: 'fill' | 'outline';
 }
 
-export const NavigationMarker = forwardRef(
-  ({ isCurrent, appearance, ...restProps }: PropsWithChildren<NavigationMarkerProps>) => {
-    switch (appearance) {
-      case 'outline':
-        return (
-          <svg
-            className={css('utrecht-navigation__marker', 'utrecht-navigation__marker--outline', {
-              'utrecht-navigation__marker--current': isCurrent,
-            })}
-            xmlns="http://www.w3.org/2000/svg"
-            width="8"
-            height="8"
-            fill="none"
-            {...restProps}
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M1.879 6.121A3 3 0 1 1 6.12 1.88 3 3 0 0 1 1.88 6.12Z"
-            />
-          </svg>
-        );
-      default:
-        return (
-          <svg
-            className={css('utrecht-navigation__marker', 'utrecht-navigation__marker--fill', {
-              'utrecht-navigation__marker--current': isCurrent,
-            })}
-            viewBox="0 0 100 100"
-            width="8"
-            height="8"
-            xmlns="http://www.w3.org/2000/svg"
-            {...restProps}
-          >
-            <circle cx="50" cy="50" r="50" fill="currentColor" />
-          </svg>
-        );
-    }
-  },
-);
+export const NavigationMarker = ({ isCurrent, appearance, ...restProps }: PropsWithChildren<NavigationMarkerProps>) => {
+  switch (appearance) {
+    case 'outline':
+      return (
+        <svg
+          className={css('utrecht-navigation__marker', 'utrecht-navigation__marker--outline', {
+            'utrecht-navigation__marker--current': isCurrent,
+          })}
+          xmlns="http://www.w3.org/2000/svg"
+          width="8"
+          height="8"
+          fill="none"
+          {...restProps}
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M1.879 6.121A3 3 0 1 1 6.12 1.88 3 3 0 0 1 1.88 6.12Z"
+          />
+        </svg>
+      );
+    default:
+      return (
+        <svg
+          className={css('utrecht-navigation__marker', 'utrecht-navigation__marker--fill', {
+            'utrecht-navigation__marker--current': isCurrent,
+          })}
+          viewBox="0 0 100 100"
+          width="8"
+          height="8"
+          xmlns="http://www.w3.org/2000/svg"
+          {...restProps}
+        >
+          <circle cx="50" cy="50" r="50" fill="currentColor" />
+        </svg>
+      );
+  }
+};
 
 NavigationMarker.displayName = 'NavigationMarker';
 NavigationMarker.defaultProps = {
