50aab3d9559a8c15ecda94d4e3ba49e1fbdfdea6
Ali Amori Kadhim <ali@frameless.io>
2024-08-15 11:01:02 +0200

    fix(ui-jest): allow overriding of transformIgnorePatterns
    work around https://github.com/vercel/next.js/issues/35634

--- packages/ui/jest.config.ts
+++ packages/ui/jest.config.ts
@@ -10,6 +10,18 @@ const config: Config = {
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
+const esModules = ['react-markdown'].join('|');
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(config);
+// work around https://github.com/vercel/next.js/issues/35634
+async function hackJestConfig() {
+  // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+  const nextJestConfig = await createJestConfig(config)();
+  // /node_modules/ is the first pattern, so overwrite it with the correct version
+  nextJestConfig.transformIgnorePatterns = nextJestConfig.transformIgnorePatterns?.map((x) => {
+    return x === '/node_modules/' ? `/node_modules/(?!(${esModules}))/` : x;
+  });
+
+  return nextJestConfig;
+}
+
+export default hackJestConfig;
