ed2544ec63de44a0a78e6c7805ef270d1bd90849
Ali Amori Kadhim <ali@frameless.io>
2024-10-09 13:48:46 +0200

    refactor: refactor kennisbank-api server

--- apps/kennisbank-api/src/queries/index.ts
+++ apps/kennisbank-api/src/queries/index.ts
@@ -1,6 +1,6 @@
 const gql = (query: any) => query;
-export const SEARCH_PRODUCT = gql(`
-    query searchProduct($locale: I18NLocaleCode) {
+export const GET_ALL_PRODUCTS = gql(`
+    query getAllProducts($locale: I18NLocaleCode) {
         products(
         locale: $locale, 
         pagination: {start: 0, limit: -1}, 
@@ -57,4 +57,63 @@ export const SEARCH_PRODUCT = gql(`
           }
         }
       }
+`);
+
+export const GET_PRODUCT_BY_UUID = gql(`
+  query getProductByUUID($locale: I18NLocaleCode, $uuid: String) {
+      products(
+      locale: $locale, 
+      filters: { uuid: { eq: $uuid }}
+      ) {
+        data {
+          id
+          attributes {
+            title
+            slug
+            uuid
+            locale
+            updatedAt
+            createdAt
+            locale
+            metaTags {
+              keymatch
+              title
+              description
+            }
+            sections {
+              ... on ComponentComponentsUtrechtRichText {
+                id
+                content
+                kennisartikelCategorie
+                component:__typename
+              }
+            }
+            catalogiMeta {
+              abstract
+              spatial {
+                scheme
+                resourceIdentifier
+              }
+              authority {
+                scheme
+                resourceIdentifier
+              }
+              audience {
+                id
+                type
+              }
+              onlineRequest {
+                type
+              }
+            }
+            pdc_metadata {
+              uplProductNaam
+              productCode
+              uitvoeringsorganisatie
+              doelgroep
+            }
+          }
+        }
+      }
+    }
 `);
