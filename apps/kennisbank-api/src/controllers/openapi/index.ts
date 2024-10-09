e576d3034bf6b224555a3eda85a789f6d5f79898
Ali Amori Kadhim <ali@frameless.io>
2024-10-09 13:48:46 +0200

    refactor: refactor kennisbank-api server

--- apps/kennisbank-api/src/controllers/openapi/index.ts
+++ apps/kennisbank-api/src/controllers/openapi/index.ts
@@ -2,21 +2,36 @@ import type { RequestHandler } from 'express';
 import yaml from 'js-yaml';
 import fs from 'node:fs';
 import path from 'node:path';
+import { getTheServerURL } from '../../utils';
 
-export const openAPIController: RequestHandler = async (_req, res, next) => {
+type Server = {
+  url: string;
+  description: string;
+};
+
+export const openAPIController: RequestHandler = async (req, res, next) => {
   try {
     // eslint-disable-next-line no-undef
     const openAPIDocument: any = yaml.load(fs.readFileSync(path.join(__dirname, '../../docs/openapi.yaml'), 'utf8'));
-    const openapiServers = openAPIDocument.servers.map((server: any) => {
-      return {
-        url: new URL('api/v1', process.env.STRAPI_PRIVATE_URL).href,
-        description: server.description,
-      };
-    });
+    const openapiComponents = {
+      schemas: {
+        kennisartikel: {
+          $ref: new URL('api/v1/objecttypes/kennisartikel', getTheServerURL(req)),
+        },
+        vac: {
+          $ref: new URL('api/v1/objecttypes/vac', getTheServerURL(req)),
+        },
+      },
+    };
+
+    const openapiServers = openAPIDocument.servers.map((server: Server) => ({
+      url: new URL('api/v1', getTheServerURL(req)),
+      description: server.description,
+    }));
     res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.status(200);
-    return res.json({ ...openAPIDocument, servers: openapiServers });
+    return res.json({ ...openAPIDocument, servers: openapiServers, components: openapiComponents });
   } catch (error) {
     next(error);
     return null;
--- apps/kennisbank-api/src/routers/kennisartikel/index.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { kennisartikelController } from '../../controllers';
-
-const router = express.Router({ mergeParams: true });
-
-router.get('/kennisartikel', kennisartikelController);
-
-export default router;
