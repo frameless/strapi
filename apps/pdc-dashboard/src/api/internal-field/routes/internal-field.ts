c312f3078ab0bcaf84ce54e3737a648360e69f89
Ali Amori Kadhim <ali@frameless.io>
2024-11-14 12:56:17 +0100

    feat: add internal fields collection

---
+++ apps/pdc-dashboard/src/api/internal-field/routes/internal-field.ts
@@ -0,0 +1,7 @@
+/**
+ * internal-field router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::internal-field.internal-field');
---
+++ apps/pdc-dashboard/src/api/internal-field/services/internal-field.ts
@@ -0,0 +1,7 @@
+/**
+ * internal-field service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::internal-field.internal-field');
