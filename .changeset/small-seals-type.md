4e5a1aa8036ff608d3ee5b5f7b07b52affdda540
Ali Amori Kadhim <ali@frameless.io>
2025-01-17 15:56:58 +0100

    fix: supports the openform HTML with nl-design-system

---
+++ .changeset/small-seals-type.md
@@ -0,0 +1,5 @@
+---
+"@frameless/pdc-frontend": patch
+---
+
+Render de Open Formulieren HTML met NL Design System CSS componenten.
--- apps/pdc-frontend/src/components/OpenFormsEmbed/OpenFormsEmbed.tsx
+++ apps/pdc-frontend/src/components/OpenFormsEmbed/OpenFormsEmbed.tsx
@@ -1,7 +1,9 @@
 import React, { type ReactNode, useId } from 'react';
 import { OpenFormsScript } from './OpenFormsScript';
 import '@open-formulieren/sdk/styles.css';
+import '@utrecht/component-library-css/dist/html.css';
 import './OpenFormsEmbed.scss';
+import { RichText } from '../index';
 
 export type OpenFormsEmbedProps = {
   nonce: string;
@@ -17,12 +19,14 @@ export const OpenFormsEmbed = ({ nonce, basePath, slug, apiUrl, sdkUrl, cssUrl, 
   const id = useId();
 
   return (
-    <>
-      <div id={id} data-base-url={apiUrl} data-form-id={slug} data-base-path={basePath}>
-        {fallback}
-      </div>
-      <link rel="stylesheet" nonce={nonce} href={cssUrl} />
-      <OpenFormsScript targetId={id} nonce={nonce} src={sdkUrl} />
-    </>
+    <div className="utrecht-html">
+      <RichText>
+        <div id={id} data-base-url={apiUrl} data-form-id={slug} data-base-path={basePath}>
+          {fallback}
+        </div>
+        <link rel="stylesheet" nonce={nonce} href={cssUrl} />
+        <OpenFormsScript targetId={id} nonce={nonce} src={sdkUrl} />
+      </RichText>
+    </div>
   );
 };
