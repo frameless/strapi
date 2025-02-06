dd504e1cac8874ddfe55cefdd9ff9a176af80501
Ali Amori Kadhim <ali@frameless.io>
2025-02-06 11:51:58 +0100

    feat: add nl-design-system to the preview HTML page

--- apps/overige-objecten-api/src/utils/createHTMLFiles.tsx
+++ apps/overige-objecten-api/src/utils/createHTMLFiles.tsx
@@ -1,4 +1,4 @@
-import { Document, Heading, Page, PageContent, Separator } from '@utrecht/component-library-react';
+import { Button, Document, Heading, Page, PageContent, Separator } from '@utrecht/component-library-react';
 import fs from 'node:fs';
 import path from 'node:path';
 import React from 'react';
@@ -6,7 +6,6 @@ import { renderToString } from 'react-dom/server';
 import { Markdown } from '../components/Markdown';
 import type { Price } from '../strapi-product-type';
 
-const categoryStyle = { border: '1px solid #ccc', padding: '10px', marginBottom: '10px' };
 interface CategoryTypes {
   [key: string]: string;
 }
@@ -21,77 +20,77 @@ const Category = ({ data, priceData }: CategoryProps) => {
   return (
     <>
       {data?.tekst && (
-        <div style={categoryStyle}>
+        <div>
           <Heading level={2}>Categorie - Tekst</Heading>
           <Separator />
           <Markdown {...markdownProps}>{data?.tekst}</Markdown>
         </div>
       )}
       {data?.procedureBeschrijving && (
-        <div style={categoryStyle}>
+        <div>
           <Heading level={2}>Categorie - Procedure Beschrijving</Heading>
           <Separator />
           <Markdown {...markdownProps}>{data?.procedureBeschrijving}</Markdown>
         </div>
       )}
       {data?.bewijs && (
-        <div style={categoryStyle}>
+        <div>
           <Heading level={2}>Categorie - Bewijs</Heading>
           <Separator />
           <Markdown {...markdownProps}>{data?.bewijs}</Markdown>
         </div>
       )}
       {data?.uitvoeringsinstructies && (
-        <div style={categoryStyle}>
+        <div>
           <Heading level={2}>Categorie - Uitvoeringsinstructies</Heading>
           <Separator />
           <Markdown {...markdownProps}>{data?.uitvoeringsinstructies}</Markdown>
         </div>
       )}
       {data?.bezwaarEnBeroep && (
-        <div style={categoryStyle}>
+        <div>
           <Heading level={2}>Categorie - Bezwaar</Heading>
           <Separator />
           <Markdown {...markdownProps}>{data?.bezwaarEnBeroep}</Markdown>
         </div>
       )}
       {data?.vereisten && (
-        <div style={categoryStyle}>
+        <div>
           <Heading level={2}>Categorie - Vereisten</Heading>
           <Separator />
           <Markdown {...markdownProps}>{data?.vereisten}</Markdown>
         </div>
       )}
       {data?.kostenEnBetaalmethoden && (
-        <div style={categoryStyle}>
+        <div>
           <Heading level={2}>Categorie - Kosten en betaalmethoden</Heading>
           <Separator />
           <Markdown {...markdownProps}>{data?.kostenEnBetaalmethoden}</Markdown>
         </div>
       )}
       {data?.contact && (
-        <div style={categoryStyle}>
+        <div>
           <Heading level={2}>Categorie - Contact</Heading>
           <Separator />
           <Markdown {...markdownProps}>{data?.contact}</Markdown>
         </div>
       )}
       {data?.wtdBijGeenReactie && (
-        <div style={categoryStyle}>
+        <div>
           <Heading level={2}>Categorie - WTD Bij Geen Reactie</Heading>
           <Separator />
           <Markdown {...markdownProps}>{data?.wtdBijGeenReactie}</Markdown>
         </div>
       )}
       {data?.notice && (
-        <div style={categoryStyle}>
+        <div>
           <Heading level={2}>Categorie - Notice</Heading>
           <Separator />
           <Markdown {...markdownProps}>{data?.notice}</Markdown>
         </div>
       )}
       {data?.deskMemo && (
-        <div style={categoryStyle}>
+        <div>
           <Heading level={2}>Categorie - Desk Memo (Interne velden)</Heading>
           <Separator />
           <Markdown {...markdownProps}>{data?.deskMemo}</Markdown>
@@ -103,44 +102,74 @@ const Category = ({ data, priceData }: CategoryProps) => {
 
 interface PageComponentProps extends CategoryProps {}
 
-const PageComponent = ({ data, priceData }: PageComponentProps) => (
-  <html lang="nl">
-    <head>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="stylesheet" href="https://unpkg.com/@utrecht/component-library-css@1.0.2-alpha.0/dist/index.css" />
-      <link rel="stylesheet" href="https://unpkg.com/@utrecht/component-library-css@1.0.2-alpha.0/dist/html.css" />
-      <link rel="stylesheet" href="https://unpkg.com/@utrecht/design-tokens@1.0.0-alpha.601/dist/index.css" />
-      <script
-        src="https://unpkg.com/@utrecht/web-component-library-stencil/dist/utrecht/utrecht.esm.js"
-        type="module"
-      ></script>
-      <style>
-        {`
+const PageComponent = ({ data, priceData }: PageComponentProps) => {
+  return (
+    <html lang="nl">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="https://unpkg.com/@utrecht/component-library-css@7.0.0/dist/index.css" />
+        <link rel="stylesheet" href="https://unpkg.com/@utrecht/component-library-css@7.0.0/dist/html.css" />
+        <link rel="stylesheet" href="https://unpkg.com/@utrecht/design-tokens@2.4.0/dist/index.css" />
+        <script
+          src="https://unpkg.com/@utrecht/web-component-library-stencil@3.0.1/dist/utrecht/utrecht.esm.js"
+          type="module"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                document.addEventListener('DOMContentLoaded', function () {
+                  const button = document.querySelector('button');
+                  const html = document.querySelector('html');
+
+                  if (button) {
+                    button.textContent = html.classList.contains('utrecht-theme') ? 'Disable Theme' : 'Enable Theme';
+
+                    button.addEventListener('click', function () {
+                      if (html) {
+                        html.classList.toggle('utrecht-theme');
+                        html.classList.toggle('utrecht-html');
+
+                        // Update button text based on the presence of the class
+                        if (html.classList.contains('utrecht-theme')) {
+                          button.textContent = 'Disable Theme';
+                        } else {
+                          button.textContent = 'Enable Theme';
+                        }
+                      }
+                    });
+                  }
+                });
+          `,
+          }}
+        />
+        <style>
+          {`
         :root {
           --utrecht-space-around: 1;
           }
           img {
-            max-width: 100%;
-            block-size: auto;
+          max-inline-size: 100%;
           }
         `}
-      </style>
-      <title>Kennisartikelen Categories</title>
-    </head>
-    <body>
-      <Document className="utrecht-theme">
-        <Page>
-          <PageContent>
-            <Heading level={1}>Kennisartikelen Categories</Heading>
-            <Separator />
-            <Category data={data} priceData={priceData} />
-          </PageContent>
-        </Page>
-      </Document>
-    </body>
-  </html>
-);
+        </style>
+        <title>Kennisartikelen Categories</title>
+      </head>
+      <body>
+        <Document>
+          <Page>
+            <PageContent>
+              <Button>Met NL design system</Button>
+              <Heading level={1}>Kennisartikelen Categories</Heading>
+              <Separator />
+              <Category data={data} priceData={priceData} />
+            </PageContent>
+          </Page>
+        </Document>
+      </body>
+    </html>
+  );
+};
 
 export const createHTMLFiles = (data: CategoryTypes, priceData: Price[]) => {
   const outputDir = './tmp/output-html';
