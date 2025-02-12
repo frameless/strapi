fd9ef755117c72c843e3acdd0bcd8cd02bfb0cf6
Ali Amori Kadhim <ali@frameless.io>
2025-02-12 11:45:05 +0100

    feat(preview-button): enable preview plugin to work with additional information collection.

---
+++ .changeset/brown-seahorses-rhyme.md
@@ -0,0 +1,5 @@
+---
+"@frameless/preview-button": minor
+---
+
+Schakel de preview-plugin in om met de aanvullende-informatie collectie te werken.
--- packages/preview-button/admin/src/utils/getContentByType.js
+++ packages/preview-button/admin/src/utils/getContentByType.js
@@ -10,20 +10,16 @@
  * @param {string} params.type - The type of content.
  * @param {JSX} params.internalField - The internal content data.
  * @param {JSX} params.vac - The vac content data.
+ * @param {JSX} params.additionalInformation - The additional information content data.
  * @returns {ContentData|null} The current content data or null.
  */
 
-export const getContentByType = ({ vac, internalField, type }) => {
-  switch (type) {
-    case 'vac':
-      return {
-        data: vac,
-      };
-    case 'internal-field':
-      return {
-        data: internalField,
-      };
-    default:
-      return null;
-  }
+export const getContentByType = ({ vac, internalField, additionalInformation, type }) => {
+  const dataMap = {
+    vac,
+    'internal-field': internalField,
+    'additional-information': additionalInformation,
+  };
+
+  return dataMap[type] ? { data: dataMap[type] } : null;
 };
