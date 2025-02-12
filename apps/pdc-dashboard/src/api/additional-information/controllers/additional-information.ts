f92a35f1a4f0f64caa7d41ac2c1a0fda642302ce
Ali Amori Kadhim <ali@frameless.io>
2025-02-12 11:27:24 +0100

    feat(pdc-dashboard): add additional-information collection to PDC-dashboard

---
+++ apps/pdc-dashboard/src/api/additional-information/controllers/additional-information.ts
@@ -0,0 +1,7 @@
+/**
+ * additional-information controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::additional-information.additional-information');
---
+++ apps/pdc-dashboard/src/api/additional-information/routes/additional-information.ts
@@ -0,0 +1,7 @@
+/**
+ * additional-information router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::additional-information.additional-information');
