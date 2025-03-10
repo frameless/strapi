1a640c5cf26e38ea42b7694c8ad2a98fadb318b0
Ali Amori Kadhim <ali@frameless.io>
2025-03-10 12:15:14 +0100

    chore(strapi-admin-extensions): integrate utils package

--- apps/strapi-admin-extensions/src/utils/index.ts
+++ apps/strapi-admin-extensions/src/utils/index.ts
@@ -1,5 +1,3 @@
 export { ErrorHandler } from './errorHandler';
-export { fetchData } from './fetchData';
 export { processCsvFile } from './processCsvFile';
-export { envAvailability } from './envAvailability';
 export { sanitizeHTML } from './sanitizeHTML';
