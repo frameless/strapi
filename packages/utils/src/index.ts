17c4309c21c4e7ce1bb709a7c1a0da108cbfa7e7
Ali Amori Kadhim <ali@frameless.io>
2025-03-13 14:33:49 +0100

    feat(utils): move getDirectionFromLanguageCode to utils

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,4 +1,5 @@
 export { envAvailability } from './envAvailability';
 export { ErrorHandler } from './errorHandler';
 export { fetchData } from './fetchData';
+export { getDirectionFromLanguageCode } from './getDirectionFromLanguageCode';
 export { getStrapiPluginName } from './get-strapi-plugin-name';
