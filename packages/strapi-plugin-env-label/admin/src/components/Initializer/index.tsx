bdd6f07c35537dbd6456c443c50e81347c9c7b71
Robbert Broersma <git@robbertbroersma.nl>
2025-08-03 21:31:27 +0200

    fix: typescript type

--- packages/strapi-plugin-env-label/admin/src/components/Initializer/index.tsx
+++ packages/strapi-plugin-env-label/admin/src/components/Initializer/index.tsx
@@ -1,7 +1,11 @@
 import { useEffect, useRef } from 'react';
 import { pluginId } from '../../pluginId';
 
-export const Initializer = ({ setPlugin }) => {
+type InitializerProps = {
+  setPlugin: (id: string) => void;
+};
+
+export const Initializer = ({ setPlugin }: InitializerProps) => {
   const ref = useRef<any>(null);
   ref.current = setPlugin;
   useEffect(() => {
