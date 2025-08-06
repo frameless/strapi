d0bb6734a540e3918f94531d74e6b8b473fee384
Ali Amori Kadhim <ali@frameless.io>
2025-08-06 12:30:53 +0200

    chore: use strapi private URL for the images

--- apps/pdc-frontend/src/util/getImageBaseUrl.ts
+++ apps/pdc-frontend/src/util/getImageBaseUrl.ts
@@ -3,7 +3,7 @@ import { buildURL } from '@frameless/utils';
 export const getImageBaseUrl = (segment?: string): string => {
   const url = buildURL({
     env: process.env,
-    key: 'STRAPI_PUBLIC_URL',
+    key: 'STRAPI_PRIVATE_URL',
     segments: segment ? [segment] : [],
   });
   if (!url) throw new Error('getImageBaseUrl: Failed to build image URL');
