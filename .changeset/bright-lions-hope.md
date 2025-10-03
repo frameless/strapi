f7d87001f02b334dfa0cdf42a2cedeaa97883bc5
Ali Amori Kadhim <ali@frameless.io>
2025-10-03 14:26:23 +0200

    feat(overige-objecten-api): use `<figure> HTML tag to indent Spotlight text block

---
+++ .changeset/bright-lions-hope.md
@@ -0,0 +1,7 @@
+---
+"@frameless/overige-objecten-api": minor
+---
+
+We hebben de Spotlight bijgewerkt zodat het tekstblok nu netjes ingesprongen wordt. Dit is bereikt door een standaard HTML-element te gebruiken, waardoor de inhoud overzichtelijker en beter leesbaar is.
+
+([GitHub Issue Frameless/strapi#1032](https://github.com/frameless/strapi/issues/1032))
--- apps/overige-objecten-api/src/utils/convertSpotlightToHTML.tsx
+++ apps/overige-objecten-api/src/utils/convertSpotlightToHTML.tsx
@@ -8,12 +8,16 @@ interface convertSpotlightToHTMLProps {
     logoButton: LogoButtonItemType[];
   };
 }
-
+/* 
+  Using <figure> to get a default border since CSS is not allowed in the receiving system.
+  This is technically a semantic misuse of <figure>, which is normally for self-contained media or illustrations.
+  Adding role="group" improves accessibility by indicating this is a related group of content and interactive elements.
+*/
 export const convertSpotlightToHTML = (item: convertSpotlightToHTMLProps['item']) => {
   return renderToString(
-    <section>
+    <figure role="group">
       <Markdown>{item?.content}</Markdown>
-      {item?.logoButton && item.logoButton.map((btn, index: number) => <BasicLogoButton item={btn} key={index} />)}
-    </section>,
+      {item?.logoButton && item.logoButton.map((btn, index) => <BasicLogoButton item={btn} key={index} />)}
+    </figure>,
   );
 };
