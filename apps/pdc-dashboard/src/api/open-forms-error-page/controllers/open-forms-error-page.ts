408a75cc9134834f7d97ffd5b6995c6f78364ab5
Marwa <marwahello@frameless.io>
2025-10-09 11:38:44 +0200

    fix: eslint run

--- apps/pdc-dashboard/src/api/open-forms-error-page/controllers/open-forms-error-page.ts
+++ apps/pdc-dashboard/src/api/open-forms-error-page/controllers/open-forms-error-page.ts
@@ -2,6 +2,6 @@
  * open-forms-error-page controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
 export default factories.createCoreController('api::open-forms-error-page.open-forms-error-page');
