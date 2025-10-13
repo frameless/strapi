57a81861be79a3b1ae4fc6a0c08db2d8e3f43df6
Ali Amori Kadhim <ali@frameless.io>
2025-10-13 17:56:12 +0200

    fix: resolve Docker build failure and improve CI pipeline
    - Fix import order in pdc-frontend error page component
    - Move lint step before build in GitHub Actions to catch errors early

--- apps/pdc-frontend/src/app/[locale]/(openFormsLayout)/form/error/[errorKey]/page.tsx
+++ apps/pdc-frontend/src/app/[locale]/(openFormsLayout)/form/error/[errorKey]/page.tsx
@@ -1,8 +1,8 @@
 import Link from 'next/link';
 import { useTranslation } from '@/app/i18n';
 import { Breadcrumbs, Heading, Markdown } from '@/components';
-import { fetchData, getStrapiGraphqlURL } from '@/util';
 import { GET_OPEN_FORMS_ERROR_PAGE } from '@/query';
+import { fetchData, getStrapiGraphqlURL } from '@/util';
 import { GetOpenFormsErrorPageQuery } from '../../../../../../../gql/graphql';
 
 type ParamsType = {
