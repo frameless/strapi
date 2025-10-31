840eefef30783d07cd573bd3565d78da71db9ba7
Ali Amori Kadhim <ali@frameless.io>
2025-10-31 12:25:07 +0100

    chore: remove contact-detail content type and rename to contact-information

--- apps/pdc-dashboard/src/api/contact-detail/routes/contact-detail.ts
+++
@@ -1,7 +0,0 @@
-/**
- * contact-detail router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::contact-detail.contact-detail');
--- apps/pdc-dashboard/src/api/contact-detail/services/contact-detail.ts
+++
@@ -1,7 +0,0 @@
-/**
- * contact-detail service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::contact-detail.contact-detail');
