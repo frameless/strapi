1410f911a2c36181dbff9c7d42ec8038099f539e
Ali Amori Kadhim <ali@frameless.io>
2026-03-09 15:16:27 +0100

    chore: strapi-plugin-flo-legal-embed
    this plugin supports only Strapi v4, and since we are migrating to Strapi v5, we need to remove it.

--- packages/strapi-plugin-flo-legal-embed/admin/src/constants.ts
+++
@@ -1,3 +0,0 @@
-import { pluginConfig } from './utils/pluginConfig';
-
-export const RESOLVE_CONFIG = `${pluginConfig.pluginId}/resolve-config`;
--- packages/strapi-plugin-flo-legal-embed/admin/src/utils/getTrad.ts
+++
@@ -1,5 +0,0 @@
-import { pluginConfig } from './pluginConfig';
-
-const getTrad = (id: string): string => `${pluginConfig.pluginId}.${id}`;
-
-export default getTrad;
