a4842b4d2b36b4277828a0e492c410c7a4158375
Ali Amori Kadhim <ali@frameless.io>
2026-03-09 15:18:39 +0100

    chore: remove strapi-plugin-scheme-select
    this plugin supports only Strapi v4, and since we are migrating to Strapi v5, we need to remove it.

--- packages/strapi-plugin-scheme-select/README.md
+++
@@ -1,3 +0,0 @@
-# Strapi plugin scheme-select
-
-A strapi custom field for selecting a scheme
--- packages/strapi-plugin-scheme-select/admin/src/pluginId.js
+++
@@ -1,5 +0,0 @@
-const pluginPkg = require('../../package.json');
-
-const pluginId = pluginPkg.name.replace(/^@frameless\/(@[^-,.][\w,-]+\/|strapi-)plugin-/i, '');
-
-module.exports = pluginId;
