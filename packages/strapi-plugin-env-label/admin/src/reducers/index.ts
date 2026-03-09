8bcf371bfbe81e76bb2857b6aa55ccd0f18418cd
Ali Amori Kadhim <ali@frameless.io>
2026-03-09 15:14:31 +0100

    chore: strapi-plugin-env-label
    this plugin supports only Strapi v4, and since we are migrating to Strapi v5, we need to remove it.

--- packages/strapi-plugin-env-label/admin/src/reducers/index.ts
+++
@@ -1,8 +0,0 @@
-import config from './config';
-import { pluginId } from '../pluginId';
-
-const reducers = {
-  [`${pluginId}_config`]: config,
-};
-
-export default reducers;
--- packages/strapi-plugin-env-label/strapi-server.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-// eslint-disable-next-line no-undef
-module.exports = require('./dist/server');
