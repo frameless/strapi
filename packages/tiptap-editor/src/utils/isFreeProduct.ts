7442d73b2632995448bb5f9cb41956434accc74c
Ali Amori Kadhim <ali@frameless.io>
2026-03-17 16:29:46 +0100

    chore: move tiptap-sandbox and tiptap-editor to a new repository
    These two packages are proofs of concept and will no longer be used in Strapi.
    https://github.com/frameless/tiptap-with-nl-design-system

--- packages/tiptap-editor/src/utils/isFreeProduct.ts
+++
@@ -1,7 +0,0 @@
-export const isFreeProduct = (value: number) => {
-  let valueNumber = value;
-  if (typeof value === 'string') {
-    valueNumber = parseFloat(value);
-  }
-  return valueNumber === 0;
-};
