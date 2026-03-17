431594ef3c472fe67238cc18431f9f8c21ed70cd
Ali Amori Kadhim <ali@frameless.io>
2026-03-17 14:20:32 +0100

    chore(vth-frontend): fix the eslint and migrate to pnpm

--- apps/vth-frontend/eslint.config.mjs
+++ apps/vth-frontend/eslint.config.mjs
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-unresolved */
-import { nextJsConfig } from "@frameless/eslint-config/next-js";
+import { nextJsConfig } from '@frameless/eslint-config/next-js';
 
 /** @type {import("eslint").Linter.Config} */
 export default nextJsConfig;
