0b29982116a3318723da95a329eb42bf6e36bb7f
Ali Amori Kadhim <ali@frameless.io>
2026-03-23 11:41:08 +0100

    test(overige-objecten-api): fix the jest issue

--- apps/overige-objecten-api/jest.config.ts
+++ apps/overige-objecten-api/jest.config.ts
@@ -1,23 +1,32 @@
 import type { Config } from 'jest';
+import { createDefaultEsmPreset } from 'ts-jest';
+
+const esModules = ['@utrecht\\+component-library-react', '@utrecht\\+components-core'].join('|');
+
+const normalModules = esModules.replace(/\\\+/g, '/');
+
+const presetConfig = createDefaultEsmPreset({
+  tsconfig: {
+    jsx: 'react-jsx',
+    module: 'ES2022',
+    target: 'ESNext',
+    esModuleInterop: true,
+    allowSyntheticDefaultImports: true,
+  },
+});
 
 const config: Config = {
-  preset: 'ts-jest',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'node'],
+  ...presetConfig,
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'node', 'mjs'],
   setupFilesAfterEnv: ['<rootDir>/src/tests/jest.setup.ts'],
-  modulePaths: ['<rootDir>'],
+  modulePaths: ['<rootDir>/node_modules', '<rootDir>/../../node_modules'],
+  moduleDirectories: ['node_modules', '../../node_modules'],
   testEnvironment: 'jsdom',
   roots: ['<rootDir>/src'],
-  transform: {
-    '^.+\\.(ts|tsx)$': [
-      'ts-jest',
-      {
-        tsconfig: 'tsconfig.test.json',
-      },
-    ],
-  },
+  transformIgnorePatterns: [`/node_modules/.pnpm/(?!(${esModules})@)`, `/node_modules/(?!(\\.pnpm|${normalModules}))`],
   moduleNameMapper: {
+    '^@utrecht/(.*)$': '<rootDir>/../../node_modules/@utrecht/$1',
     uuid: '<rootDir>/src/__mocks__/uuid.ts',
   },
 };
-
 export default config;
