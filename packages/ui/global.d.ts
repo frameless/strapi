27cef4859b5f3103cecde64003d3ce48b9bb6bb5
Robbert Broersma <git@robbertbroersma.nl>
2026-04-06 13:13:37 +0200

    build: configure TypeScript to recognize .css as well as .scss

--- packages/ui/global.d.ts
+++ packages/ui/global.d.ts
@@ -1,6 +1,16 @@
 declare module 'rollup-plugin-peer-deps-external';
 
-declare module '*.scss' {
+declare module '*.module.css' {
   const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.module.scss' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.css' {
+  const content: string;
   export default content;
 }
