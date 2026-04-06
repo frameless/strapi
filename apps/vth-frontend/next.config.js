09f81b9ddc6e8d7fa5b0992f35cec78fb3c0f4ff
Ali Amori Kadhim <ali@frameless.io>
2026-04-06 14:15:24 +0200

    chore(vth-frontend): fix esModule issue

--- apps/vth-frontend/next.config.js
+++ apps/vth-frontend/next.config.js
@@ -1,5 +1,10 @@
-const { URL } = require('url');
+import path from 'path';
+import { URL, fileURLToPath } from 'node:url';
+
 const { hostname, protocol, port } = new URL(process.env.STRAPI_PUBLIC_URL || 'http://localhost:1337');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@frameless/ui', '@utrecht/component-library-react', '@utrecht/web-component-library-react'],
@@ -12,9 +17,15 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.conditionNames = ['import', 'require', 'default'];
+    }
+    return config;
+  },
   experimental: {
     serverActions: true,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
