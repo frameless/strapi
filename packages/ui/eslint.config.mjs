f8d1771f6978e21b9c4edc8d366abf45bc5eff67
Ali Amori Kadhim <ali@frameless.io>
2026-04-17 13:52:56 +0200

    chore: update linting, build tooling and rollup terser plugin
    - add missing eslint config to additional apps
    - fix lint-staged issues in lint-build workflow
    - resolve minor eslint rule violations
    - migrate from rollup-plugin-terser to @rollup/plugin-terser
      ([legacy plugin is archived and unmaintained since 2020](https://github.com/TrySound/rollup-plugin-terser))

--- packages/ui/eslint.config.mjs
+++ packages/ui/eslint.config.mjs
@@ -1,4 +1,18 @@
 import { reactConfig } from '@frameless/eslint-config/react-internal';
 
-/** @type {import("eslint").Linter.Config} */
-export default reactConfig;
+/** @type {import("eslint").Linter.Config[]} */
+export default [
+  ...reactConfig,
+
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    languageOptions: {
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      'no-undef': 'off',
+    },
+  },
+];
